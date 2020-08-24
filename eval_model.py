500b0cbe7913a7eb0310ade827d3156eb02fd8d3
MClarkTurner <mclarktu@fandm.edu>
2020-08-24 10:55:04 -0400

    eval model check

--- eval_model.py
+++ eval_model.py
@@ -10,7 +10,7 @@ def eval(lfd_params, net):
 		lfd_params.file_directory, 
 		"evaluation", 
 		max_length=lfd_params.args.max_length,
-		num_workers=2)
+		num_workers=1)
 
 	# Build Network
 	#----------------
