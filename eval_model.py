cc614a0f73fd2095a84957e07ea5caba55af5729
MClarkTurner <mclarktu@fandm.edu>
2020-08-26 09:59:04 -0400

    saptial feature eval

--- eval_model.py
+++ eval_model.py
@@ -1,5 +1,7 @@
 import sys
 import torch
+import numpy as np
+import pandas as pd
 
 def eval(lfd_params, net):
 
--- model/model.py
+++ model/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class LfDNetwork(nn.Module):
 	def __init__(self, lfd_params, is_training=False):
