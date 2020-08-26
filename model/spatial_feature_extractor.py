739f0cd82a3010f4d6e658c73c71aa17d6ed8b80
MClarkTurner <mclarktu@fandm.edu>
2020-08-26 09:40:57 -0400

    saptial feature eval

--- model/spatial_feature_extractor.py
+++ model/spatial_feature_extractor.py
@@ -53,14 +53,15 @@ class SpatialFeatureExtractor(nn.Module):
 		# pass data through CNNs
 		rgb_y = self.rgb_net(rgb_x)
 		
-		print("rgb_y:", rgb_y.shape)
-		
+		print("rgb_y 1:", rgb_y.shape)
+
 
 		# apply linear layer and consensus module to the output of the CNN
 		if (self.is_training):
 			rgb_y = rgb_y.view((-1, self.rgb_net.num_segments) + rgb_y.size()[1:])
 		if (self.is_training):
 			rgb_y = rgb_y.view((-1, self.rgb_net.num_segments*10) + rgb_y.size()[1:])
+		print("rgb_y 2:", rgb_y.shape)
 		rgb_y = self.consensus(rgb_y)
 		rgb_y = rgb_y.squeeze(1)
 
