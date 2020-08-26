1ed8bf5e5e2c0796b1410a2b1b1bd7b4aa1f2069
MClarkTurner <mclarktu@fandm.edu>
2020-08-26 09:14:52 -0400

    saptial feature eval

--- video_dataset.py
+++ video_dataset.py
@@ -120,8 +120,9 @@ class VideoDataset(Dataset):
 		# collect array of frames into list
 		images = []
 		for start_idx in start_indexes:
-			print("start_idx:", start_idx, stride, total_num_frames-1)
-			frame_indexes = np.array([(idx * stride + start_idx) % total_num_frames-1 for idx in range(self.num_segments)])
+			print("start_idx:", start_idx, stride, total_num_frames-1, self.num_segments)
+
+			frame_indexes = np.array([(idx * stride + start_idx) % (total_num_frames-1) for idx in range(self.num_segments)])
 			print("frame_indexes1")
 			print(frame_indexes)
 			frame_indexes = np.array([(idx * stride + start_idx) % total_num_frames-1 for idx in range(self.num_segments)])+1
