c56fe5af63e3644eee090493ef3c35ae3b378981
MClarkTurner <mclarktu@fandm.edu>
2020-08-28 10:10:36 -0400

    freeze feature training

--- eval_model.py
+++ eval_model.py
@@ -27,56 +27,56 @@ def eval(lfd_params, net):
 	rec_expected_action = []
 	rec_observed_action = []
 
-	with 
+	with torch.no_grad():
 
-	for i, (obs, state, action, filename) in enumerate(eval_loader):
+		for i, (obs, state, action, filename) in enumerate(eval_loader):
 
-		# process visual observation data
-		obs = torch.reshape(obs, (-1, lfd_params.args.num_segments * 10, 3, 224,224))#obs.view(-1, max_length, 3, 224,224)
-		obs_x = torch.autograd.Variable(obs)
+			# process visual observation data
+			obs = torch.reshape(obs, (-1, lfd_params.args.num_segments * 10, 3, 224,224))#obs.view(-1, max_length, 3, 224,224)
+			obs_x = torch.autograd.Variable(obs)
 
-		# process hidden world data
-		state_x = torch.autograd.Variable(state)
+			# process hidden world data
+			state_x = torch.autograd.Variable(state)
 
-		# input shapes
-		if (i == 0):
-			print("obs_x: ", obs_x.shape)
-			print("state_x: ", state_x.shape)
-		
-		# process action label
-		action = action.cuda()
-		action_y = torch.autograd.Variable(action)
-		
-		# compute output
-		action_logits = net(obs_x, state_x)
+			# input shapes
+			if (i == 0):
+				print("obs_x: ", obs_x.shape)
+				print("state_x: ", state_x.shape)
+			
+			# process action label
+			action = action.cuda()
+			action_y = torch.autograd.Variable(action)
+			
+			# compute output
+			action_logits = net(obs_x, state_x)
 
-		action_logits = action_logits.detach().cpu().numpy()
-		action_out = np.argmax(action_logits, axis=1)
+			action_logits = action_logits.detach().cpu().numpy()
+			action_out = np.argmax(action_logits, axis=1)
 
-		for j, file in enumerate(filename):
-			# add information to DataFrame
-			rec_obs_label.append(file.split('/')[-2])
-			rec_state.append(state[j].detach().cpu().numpy())
-			rec_expected_action.append(action[j].detach().cpu().numpy())
-			rec_observed_action.append(action_out[j])
+			for j, file in enumerate(filename):
+				# add information to DataFrame
+				rec_obs_label.append(file.split('/')[-2])
+				rec_state.append(state[j].detach().cpu().numpy())
+				rec_expected_action.append(action[j].detach().cpu().numpy())
+				rec_observed_action.append(action_out[j])
 
-		if(i % 100 == 0):
-			print("iter: {:6d}/{:6d}".format(i, len(eval_loader)))
+			if(i % 100 == 0):
+				print("iter: {:6d}/{:6d}".format(i, len(eval_loader)))
 
-	# write output to file
-	import datetime
-	currentDT = datetime.datetime.now()
-	df = pd.DataFrame({
-			"obs_label":rec_obs_label,
-			"state":rec_state,
-			"expected_action":rec_expected_action,
-			"observed_action":rec_observed_action,
-		})
+		# write output to file
+		import datetime
+		currentDT = datetime.datetime.now()
+		df = pd.DataFrame({
+				"obs_label":rec_obs_label,
+				"state":rec_state,
+				"expected_action":rec_expected_action,
+				"observed_action":rec_observed_action,
+			})
 
-	model_id = lfd_params.args.modelname.split("/")[-1][len("saved_model_"):-3]
+		model_id = lfd_params.args.modelname.split("/")[-1][len("saved_model_"):-3]
 
-	out_filename = os.path.join(lfd_params.args.output_dir, "output_"+model_id+".csv")
-	df.to_csv(out_filename)
+		out_filename = os.path.join(lfd_params.args.output_dir, "output_"+model_id+".csv")
+		df.to_csv(out_filename)
 
 if __name__ == '__main__':
 
