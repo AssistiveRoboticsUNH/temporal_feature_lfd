14fc6efd36ce6725926a079ced89f31b0660943e
MClarkTurner <mclarktu@fandm.edu>
2020-08-28 14:47:26 -0400

    update scripts

--- train_model.py
+++ train_model.py
@@ -31,41 +31,42 @@ def train(lfd_params, model):
 		
 	# Train Network
 	#----------------
+	with autograd.detect_anomaly():
 
-	epoch = lfd_params.args.epochs
-	for e in range(epoch):
-		for i, data_packet in enumerate(train_loader):
+		epoch = lfd_params.args.epochs
+		for e in range(epoch):
+			for i, data_packet in enumerate(train_loader):
 
-			obs, state, action = data_packet
+				obs, state, action = data_packet
 
-			# process visual observation data
-			obs_x = torch.autograd.Variable(obs)
+				# process visual observation data
+				obs_x = torch.autograd.Variable(obs)
 
-			# process hidden world data
-			state_x = torch.autograd.Variable(state)
+				# process hidden world data
+				state_x = torch.autograd.Variable(state)
 
-			# input shapes
-			if (e == 0 and i == 0):
-				print("obs_x: ", obs_x.shape)
-				print("state_x: ", state_x.shape)
-			
-			# process action label
-			action = action.cuda()
-			action_y = torch.autograd.Variable(action)
-			
-			# compute output
-			action_logits = net(obs_x, state_x)
+				# input shapes
+				if (e == 0 and i == 0):
+					print("obs_x: ", obs_x.shape)
+					print("state_x: ", state_x.shape)
+				
+				# process action label
+				action = action.cuda()
+				action_y = torch.autograd.Variable(action)
+				
+				# compute output
+				action_logits = net(obs_x, state_x)
 
-			loss = criterion(action_logits, action_y)
+				loss = criterion(action_logits, action_y)
 
-			# compute gradient and do SGD step
-			loss.backward()
+				# compute gradient and do SGD step
+				loss.backward()
 
-			optimizer.step()
-			optimizer.zero_grad()
+				optimizer.step()
+				optimizer.zero_grad()
 
-			if(i % 100 == 0):
-				print("epoch: {:3d}/{:3d},  iter: {:6d}/{:6d}".format(e, epoch, i, len(train_loader)))
+				if(i % 100 == 0):
+					print("epoch: {:3d}/{:3d},  iter: {:6d}/{:6d}".format(e, epoch, i, len(train_loader)))
 
 	# save trained model parameters
 	out_filename = lfd_params.generate_modelname()
