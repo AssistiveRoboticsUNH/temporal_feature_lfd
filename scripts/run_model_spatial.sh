e315b8fb3ade80335d45ec480029264e6130f048
MClarkTurner <mclarktu@fandm.edu>
2020-08-28 15:11:11 -0400

    update scripts

---
+++ scripts/run_model_spatial.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+EPOCHS=10
+BATCH=16
+
+SAVE_ID="model0"
+
+python3 train_model.py bs --trim --epochs $EPOCHS --batch_size $BATCH --save_id $SAVE_ID
+python3 eval_model.py bs --trim --save_id $SAVE_ID
+
+OUTPUT_NAME="csv_output/output_"$SAVE_ID".csv"
+
+python3 analysis/action_metrics.py $OUTPUT_NAME
