898aac0f8674f7dd7f0f971fe966d4692ff92918
MClarkTurner <mclarktu@fandm.edu>
2020-08-28 11:46:44 -0400

    train model only

--- parameter_parser.py
+++ parameter_parser.py
@@ -70,7 +70,7 @@ class Parameters:
 		if(self.args.save_id==""):
 			currentDT = datetime.datetime.now()
 			use_ditrl = "ditrl_" if self.args.use_ditrl else ""
-			use_trim = "trim_" if self.args.use_ditrl else ""
+			use_trim = "trim_" if self.args.trim_model else ""
 			self.args.save_id = use_ditrl+use_trim+self.args.app+"_"+currentDT.strftime("%Y-%m-%d_%H-%M-%S")
 
 	def generate_modelname(self, section="null"):
