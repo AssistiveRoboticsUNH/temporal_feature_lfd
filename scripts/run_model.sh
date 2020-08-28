abfcbe56493ca01719666501031f777134db89ee
MClarkTurner <mclarktu@fandm.edu>
2020-08-28 14:02:58 -0400

    update scripts

--- scripts/run_model.sh
+++ scripts/run_model.sh
@@ -3,7 +3,7 @@
 SAVE_ID="model0"
 SAVE_ID_DITRL="model0_ditrl"
 
-BACKBONE_MODEL="saved_models/saved_model_"+$SAVE_ID+".backbone.pt"
+BACKBONE_MODEL="saved_models/saved_model_"$SAVE_ID".backbone.pt"
 
 echo $SAVE_ID
 echo $SAVE_ID_DITRL
@@ -13,7 +13,7 @@ echo $BACKBONE_MODEL
 python3 train_model.py bs --trim --epochs 10 --ditrl --backbone_modelname $BACKBONE_MODEL --save_id $SAVE_ID_DITRL
 
 #python3 eval_model.py bs --trim --epochs 10 --save_id $(SAVE_ID)
-python3 eval_model.py bs --trim --epochs 10 --save_id $SAVE_ID_DITRL
+#python3 eval_model.py bs --trim --epochs 10 --save_id $SAVE_ID_DITRL
 
 #python3 analysis/action_metrics.py csv_output/output_$(SAVE_ID).csv
-python3 analysis/action_metrics.py "csv_output/output_"+$SAVE_ID_DITRL+".csv"
+#python3 analysis/action_metrics.py "csv_output/output_"+$SAVE_ID_DITRL+".csv"
