df188b30fb7fa4f8f810dd7c82f5bd491ed3cde5
MClarkTurner <mclarktu@fandm.edu>
2020-08-30 09:51:47 -0400

    update scripts

--- video_dataset.py
+++ video_dataset.py
@@ -94,6 +94,8 @@ class VideoDataset(Dataset):
 		self.image_tmpl = image_tmpl 
 		self.clip_size = clip_size
 
+		print("self.num)sef:". self.num_segments)
+
 		self.full_sample = full_sample
 		self.fix_stride = fix_stride
 
