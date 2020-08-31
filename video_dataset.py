2226db4ab5a12a4f42016107e897bb6e20f068e3
MClarkTurner <mclarktu@fandm.edu>
2020-08-31 11:24:55 -0400

    tune parameters

--- video_dataset.py
+++ video_dataset.py
@@ -132,7 +132,10 @@ class VideoDataset(Dataset):
 		# collect array of frames into list
 		images = []
 
-		idxs = np.linspace(0, max(1, total_num_frames-1), num=self.num_segments, dtype=int)+1
+		if (self.mode == "train"):
+			idxs = np.linspace(0, max(1, total_num_frames-1), num=self.num_segments, dtype=int)+1
+		else:
+			idxs = np.linspace(0, max(1, total_num_frames-1), num=self.num_segments*10, dtype=int)+1
 		for idx in idxs:
 			images.append( Image.open(os.path.join(filename, self.image_tmpl.format(idx))).convert('RGB') )
 
