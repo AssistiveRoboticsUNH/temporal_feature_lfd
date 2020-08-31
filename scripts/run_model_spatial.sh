af40d642ce9e70673a29843e1696cc083fdfd472
MClarkTurner <mclarktu@fandm.edu>
2020-08-31 12:08:45 -0400

    tune parameters

--- scripts/run_model_spatial.sh
+++ scripts/run_model_spatial.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+SEGMENTS=16
 EPOCHS=100
 BATCH=9
 
 SAVE_ID="model0"
 
-python3 train_model.py bs --trim --epochs $EPOCHS --batch_size $BATCH --save_id $SAVE_ID
-python3 eval_model.py bs --trim --save_id $SAVE_ID
+python3 train_model.py bs --trim --epochs $EPOCHS --batch_size $BATCH --save_id $SAVE_ID --num_segments $SEGMENTS
+python3 eval_model.py bs --trim --save_id $SAVE_ID --num_segments $SEGMENTS
 
 OUTPUT_NAME="csv_output/output_"$SAVE_ID".csv"
 
