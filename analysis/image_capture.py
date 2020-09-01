2ec7a2c10881a06d43eeda0d88b41f4cac9ba772
MClarkTurner <mclarktu@fandm.edu>
2020-09-01 06:40:09 -0400

    tune parameters

---
+++ analysis/image_capture.py
@@ -0,0 +1,52 @@
+import sys, os
+from PIL import Image
+import numpy as np 
+
+def read_file(args, filename, image_tmpl='image_{:05d}.jpg'):
+
+	total_num_frames = len(os.listdir(filename))
+
+	# collect frames
+	images = []
+	if (args.mode == "train"):
+		idxs = np.linspace(0, max(1, total_num_frames-1), num=args.num_segments, dtype=int)+1
+	else:
+		idxs = np.linspace(0, max(1, total_num_frames-1), num=args.num_segments*10, dtype=int)+1
+	for idx in idxs:
+		images.append( Image.open(os.path.join(filename, image_tmpl.format(idx))).convert('RGB') )
+
+	# stitch frames together
+	def get_concat_h(im1, im2):
+		dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+		dst.paste(im1, (0, 0))
+		dst.paste(im2, (im1.width, 0))
+		return dst
+
+	def get_concat_v(im1, im2):
+		dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+		dst.paste(im1, (0, 0))
+		dst.paste(im2, (0, im1.height))
+		return dst
+
+	img = images[0]
+	for i in range(1, len(images)):
+		img = get_concat_h(img, images[i])
+
+	
+
+	# save to file
+
+	img.save(os.path.join())
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Generate IADs from input files')
+	parser.add_argument('input_file', help='the checkpoint file to use with the model')
+	parser.add_argument('--fig_dir', default="analysis/fig",help='the checkpoint file to use with the model')
+	parser.add_argument('--num_segments', default=8, type=int,help='the checkpoint file to use with the model')
+	parser.add_argument('--mode', default="train", choices=["train", "eval"],help='the checkpoint file to use with the model')
+	args = parser.parse_args()
+
+	src_filename = args.input_file.split("/")[-1][:-4]
+
+	read_file(args, src_filename, image_tmpl='image_{:05d}.jpg')
