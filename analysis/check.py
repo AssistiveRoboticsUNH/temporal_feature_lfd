a01b7833c9e5d59d3757060a6a63805bd225e22a
MClarkTurner <mclarktu@fandm.edu>
2020-09-01 13:02:21 -0400

    tune parameters

--- analysis/check.py
+++ analysis/check.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset, DataLoader
 class XORDataset(Dataset):
 	def __init__(self):
 		self.dataset = [ [1,1], [1,0], [0,1], [0,0] ]
-		self.labelset = [  [0],   [1],   [1],   [0] ]
+		self.labelset = [  0,   1,   1,   0 ]
 
 	def __getitem__(self, i):
 		data  = torch.tensor([self.dataset[i]], dtype=torch.float)
