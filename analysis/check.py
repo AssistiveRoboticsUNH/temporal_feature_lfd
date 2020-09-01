b2fd4b951c0f2a356840233f4c4459eaf408f470
MClarkTurner <mclarktu@fandm.edu>
2020-09-01 13:06:36 -0400

    tune parameters

--- analysis/check.py
+++ analysis/check.py
@@ -70,7 +70,7 @@ for run in range(5):
 	# Train Network
 	#----------------
 	losses = []
-	epoch = 1000
+	epoch = 200
 	with torch.autograd.detect_anomaly():
 		for e in range(epoch):
 			for n, (data, label) in enumerate(train_dl):
