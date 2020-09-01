9c1b872c004f95ea2b1985998555300e06ea0aed
MClarkTurner <mclarktu@fandm.edu>
2020-09-01 09:46:55 -0400

    tune parameters

---
+++ analysis/check.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import numpy as np 
+
+dataset = [ [1,1], [1,0], [0,1], [0,0] ]
+labelset = [ 0, 1, 1, 0 ]
+
+
+class Model:
+	def __init __(self):
+		self.lin = nn.Linear(2,1)
+
+	def forward(self, inp):
+		return self.lin(inp)
+
+net = Model()
+
+params = list(net.parameters())
+net = torch.nn.DataParallel(net, device_ids=0).cuda()
+net.train()
+
+# define loss function
+criterion = torch.nn.CrossEntropyLoss().cuda()
+
+# define optimizer
+optimizer = torch.optim.SGD(params, 0.01)
+	
+# Train Network
+#----------------
+epoch = 10
+with torch.autograd.detect_anomaly():
+	for e in range(epoch):
+		i = random.randint(0, 3)
+		data, label = dataset[i], labelset[i]
+
+		data = torch.autograd.Variable(data).cuda()
+		label = torch.autograd.Variable(label).cuda()
+		
+		# compute output
+		logits = net(data)
+
+		# get loss
+		loss = criterion(logits, label)
+		loss.backward()
+
+		# optimize SGD
+		optimizer.step()
+		optimizer.zero_grad()
+
+		print("loss:", loss.cpu().detach().numpy())
+		print("expected:", label.cpu().detach().numpy())
+		print("output:", logits.cpu().detach().numpy())
+
