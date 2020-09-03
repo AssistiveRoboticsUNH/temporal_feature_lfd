c554ddfa8c46e6e7979998412f8af887f99b33f8
MClarkTurner <mclarktu@fandm.edu>
2020-09-03 13:38:47 -0400

    video learning

--- model/backbone_model/tsm/tsm.py
+++ model/backbone_model/tsm/tsm.py
@@ -68,7 +68,7 @@ class TSMWrapper(TSN):
             print(k)
         print("vars done")
         '''
-        print(new_state_dict['state_dict']['layer4.2.conv1.net.weight'])
+        print(new_state_dict['layer4.2.conv1.net.weight'])
 
         self.base_model.load_state_dict(new_state_dict, strict=True)
         
