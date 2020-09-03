8ce811919c413ab6ca960463d121747d6d868c5c
MClarkTurner <mclarktu@fandm.edu>
2020-09-02 23:38:03 -0400

    XOR test

--- analysis/check.py
+++ analysis/check.py
@@ -9,12 +9,12 @@ from torch.utils.data import Dataset, DataLoader
 class XORDataset(Dataset):
 	def __init__(self):
 		self.dataset = [ [1,1], [1,0], [0,1], [0,0] ]
-		#self.labelset = [  0,   1,   1,   0 ]
-		self.labelset = [  [0],   [1],   [1],   [0] ]
+		self.labelset = [  0,   1,   1,   0 ]
 
 	def __getitem__(self, i):
 		data  = torch.tensor(self.dataset[i], dtype=torch.float)
-		label = torch.tensor(self.labelset[i], dtype=torch.float)
+		#label = torch.tensor(self.labelset[i], dtype=torch.float)
+		label = torch.tensor(self.labelset[i], dtype=torch.int32)
 		return data, label
 
 	def __len__(self):
