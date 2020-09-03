46c5592d7c5d51724bf49aaa0401aec8db6ba6a3
MClarkTurner <mclarktu@fandm.edu>
2020-09-03 08:31:29 -0400

    XOR test

--- video_dataset.py
+++ video_dataset.py
@@ -137,6 +137,7 @@ class VideoDataset(Dataset):
 		idxs = np.linspace(0, max(1, total_num_frames-1), num=self.num_segments, dtype=int)+1
 		#else:
 		#	idxs = np.linspace(0, max(1, total_num_frames-1), num=self.num_segments*10, dtype=int)+1
+		idxs = [idxs[4]]
 		print("idxs:", idxs)
 		for idx in idxs:
 			images.append( Image.open(os.path.join(filename, self.image_tmpl.format(idx))).convert('RGB') )
