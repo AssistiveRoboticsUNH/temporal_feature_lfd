81b9f0663b2ae44f33dd98af6907080a723495ae
MClarkTurner <mclarktu@fandm.edu>
2020-09-02 23:24:09 -0400

    XOR test

--- analysis/check.py
+++ analysis/check.py
@@ -23,7 +23,7 @@ class XORDataset(Dataset):
 class Model(nn.Module):
 	def __init__(self):
 		super().__init__()
-		'''
+		
 		self.lin1 = nn.Linear(2,2)
 		self.lin2 = nn.Linear(2,1)
 		'''
@@ -34,15 +34,15 @@ class Model(nn.Module):
 			#nn.Sigmoid()
 		)
 		#self.lin = nn.Linear(2,2)
-		
-	def forward(self, x):
 		'''
+	def forward(self, x):
+		
 		x = self.lin1(x)
 		x = F.sigmoid(x)
 		x = self.lin2(x)
 		return x
-		'''
-		return self.lin(x)
+		
+		#return self.lin(x)
 
 data_dict = {}
 
