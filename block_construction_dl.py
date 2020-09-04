cec12f51762a0053655f4a6f092cb79e15f23f1f
MClarkTurner <mclarktu@fandm.edu>
2020-09-04 10:10:19 -0400

    getting temporal model working

--- block_construction_dl.py
+++ block_construction_dl.py
@@ -80,12 +80,13 @@ class BlockConstructionDataSet(VideoDataset):
 
 		if (trim):
 			for obs_category in self.obs_dict.keys():
-				for obs_sample in self.obs_dict[obs_category]:
+				if obs_category in self.obs_dict:
+					for obs_sample in self.obs_dict[obs_category]:
 
-					obs_file_dir = os.path.join(*[root_path, obs_category, obs_sample])
+						obs_file_dir = os.path.join(*[root_path, obs_category, obs_sample])
 
-					for action in self.action_dict[obs_category]:
-						self.data.append( self.Data(obs_file_dir, 0, action) )
+						for action in self.action_dict[obs_category]:
+							self.data.append( self.Data(obs_file_dir, 0, action) )
 
 		else:
 			for obs_category in self.obs_dict.keys():
