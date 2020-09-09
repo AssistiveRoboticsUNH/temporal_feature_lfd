3b9f4168dacd879ab37abb88eb62fba058e03cc0
mbc2004 <mbc2004@cs.unh.edu>
2020-09-09 07:04:40 -0400

    update scripts to use 16 segments

--- scripts/run_model_spatial.sh
+++ scripts/run_model_spatial.sh
@@ -1,38 +1,41 @@
 #!/bin/sh
 
-SEGMENTS=8
+SEGMENTS=16
 EPOCHS=200
 ALPHA=0.0005
 BATCH=3
 
 SAVE_ID="model0"
-
 OUTPUT_NAME="csv_output/output_"$SAVE_ID".csv"
 
-echo "python3 train_model.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA
+TRAIN_CMD="python3 train_spatial.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA
+EVAL_CMD="python3 eval_model.py bs --trim --save_id "$SAVE_ID
+ANALYZE_CMD="python3 analysis/action_metrics.py "$OUTPUT_NAME
+
+echo $TRAIN_CMD
 echo ""
-echo "python3 eval_model.py bs --trim --save_id "$SAVE_ID
+echo $EVAL_CMD
 echo ""
-echo "python3 analysis/action_metrics.py "$OUTPUT_NAME
+echo $ANALYZE_CMD
 echo ""
 
 echo "====="
 echo "TRAIN"
 echo "====="
 
-echo "python3 train_model.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA
-python3 train_model.py bs --trim --epochs $EPOCHS --batch_size $BATCH --save_id $SAVE_ID --num_segments $SEGMENTS --lr $ALPHA
+echo $TRAIN_CMD
+eval $TRAIN_CMD
 
 echo "====="
 echo "EVAL"
 echo "====="
 
-echo "python3 eval_model.py bs --trim --save_id "$SAVE_ID
-python3 eval_model.py bs --trim --save_id $SAVE_ID
+echo $EVAL_CMD
+eval $EVAL_CMD
 
 echo "====="
 echo "ANALYZE"
 echo "====="
 
-echo "python3 analysis/action_metrics.py "$OUTPUT_NAME
-python3 analysis/action_metrics.py $OUTPUT_NAME
+echo $ANALYZE_CMD
+eval $ANALYZE_CMD
