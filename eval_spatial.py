72b6dc998dcf6f0d6517503b3b2b7625b510bd17
mbc2004 <mbc2004@cs.unh.edu>
2020-09-09 07:58:40 -0400

    correctly implemented eval_spatial.py

---
+++ eval_spatial.py
@@ -0,0 +1,72 @@
+"""
+This code is for the training of a network using only the backbone model
+"""
+import torch
+import numpy as np
+import pandas as pd
+import os
+
+
+def eval(lfd_params, model):
+
+    # Create DataLoaders
+    data_loader = lfd_params.create_dataloader(lfd_params, "evaluation", verbose=True)
+
+    # put model on GPU
+    net = torch.nn.DataParallel(model, device_ids=lfd_params.args.gpus).cuda()
+    net.eval()
+
+    # recorded values
+    rec_obs_label = []
+    rec_state = []
+    rec_expected_action = []
+    rec_observed_action = []
+
+    # Evaluate Network
+    with torch.no_grad():
+
+        for i, data_packet in enumerate(data_loader):
+
+            obs, state, action, filename = data_packet
+
+            # input shapes
+            if i == 0:
+                print("obs_x: ", obs.shape)
+                print("state_x: ", state.shape)
+
+            # compute output
+            action_logits = net(obs, state).detach().cpu().numpy()
+            action_out = np.argmax(action_logits, axis=1)
+
+            for j, file in enumerate(filename):
+                # add information to DataFrame
+                rec_obs_label.append(file.split('/')[-2])
+                rec_state.append(state[j].detach().cpu().numpy())
+                rec_expected_action.append(action[j].detach().cpu().numpy())
+                rec_observed_action.append(action_out[j])
+
+    # write output to file
+    df = pd.DataFrame({
+        "obs_label": rec_obs_label,
+        "state": rec_state,
+        "expected_action": rec_expected_action,
+        "observed_action": rec_observed_action,
+    })
+
+    out_filename = os.path.join(lfd_params.args.output_dir, "output_" + lfd_params.args.save_id + ".csv")
+    df.to_csv(out_filename)
+
+    print("Output placed in: " + out_filename)
+
+
+
+if __name__ == '__main__':
+
+    from parameter_parser import parse_model_args
+    lfd_params_obj = parse_model_args()
+
+    from model.spatial_feature_extractor import SpatialFeatureExtractor
+    model_obj = SpatialFeatureExtractor(lfd_params_obj, is_training=False)
+
+    eval(lfd_params_obj, model_obj)
+
--- scripts/run_model_spatial.sh
+++ scripts/run_model_spatial.sh
@@ -9,7 +9,7 @@ SAVE_ID="model0"
 OUTPUT_NAME="csv_output/output_"$SAVE_ID".csv"
 
 TRAIN_CMD="python3 train_spatial.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA
-EVAL_CMD="python3 eval_model.py bs --trim --save_id "$SAVE_ID
+EVAL_CMD="python3 eval_spatial.py bs --trim --save_id "$SAVE_ID
 ANALYZE_CMD="python3 analysis/action_metrics.py "$OUTPUT_NAME
 
 echo $TRAIN_CMD
@@ -24,7 +24,7 @@ echo "TRAIN"
 echo "====="
 
 echo $TRAIN_CMD
-eval $TRAIN_CMD
+#eval $TRAIN_CMD
 
 echo "====="
 echo "EVAL"
