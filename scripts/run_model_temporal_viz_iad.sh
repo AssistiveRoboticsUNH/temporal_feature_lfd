f36a399ce409b4ecbbdd6bb2d1c1e57d00174ffb
mbc2004 <mbc2004@cs.unh.edu>
2020-09-09 10:05:22 -0400

    Visualize IADs bug fix: executable file selection

--- scripts/run_model_temporal_viz_iad.sh
+++ scripts/run_model_temporal_viz_iad.sh
@@ -8,7 +8,7 @@ SAVE_ID_DITRL=$SAVE_ID"_ditrl"
 BACKBONE_MODEL="saved_models/saved_model_"$SAVE_ID".backbone.pt"
 OUTPUT_NAME_DITRL="csv_output/output_"$SAVE_ID_DITRL".csv"
 
-VIZUALIZE_IAD="python3 iad_visualization.py bs --trim --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --backbone_modelname "$BACKBONE_MODEL" --num_segments "$SEGMENTS
+VIZUALIZE_IAD="python3 analysis/iad_visualization.py bs --trim --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --backbone_modelname "$BACKBONE_MODEL" --num_segments "$SEGMENTS
 
 echo $VIZUALIZE_IAD
 
