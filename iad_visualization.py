d7b4918b9baff0e3af30b72ea663bd230fe623ad
mbc2004 <mbc2004@cs.unh.edu>
2020-09-09 12:17:29 -0400

    Visualize IADs bug fix: convert IAD to PIL Image

--- iad_visualization.py
+++ iad_visualization.py
@@ -67,7 +67,7 @@ def run(lfd_params, model):
                     iad_frame = Image.fromarray(iad_frame)
                     print("iad_frame 2:", iad_frame.width, iad_frame.height, frame.width)
                     print("new shape:", (512, frame.width))
-                    iad_frame.thumbnail((512, frame.width))  # , Image.ANTIALIAS)
+                    iad_frame.resize((512, frame.width))  # , Image.ANTIALIAS)
                     #iad_frame = iad_frame.resize([-1, frame.width], PIL.Image.ANTIALIAS)
                     print("iad_frame 3:", iad_frame.width, iad_frame.height)
 
