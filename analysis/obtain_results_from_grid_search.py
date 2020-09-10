a5da2443c1b2b90b7032d53a5d4ec7868ef06714
mbc2004 <mbc2004@cs.unh.edu>
2020-09-10 11:36:15 -0400

    debug obtain_results_from_grid_search.py

--- analysis/obtain_results_from_grid_search.py
+++ analysis/obtain_results_from_grid_search.py
@@ -7,14 +7,13 @@ import sys
 if __name__ == "__main__":
     # load spatial data
     spatial_df = pd.read_csv(sys.argv[1])
-    spatial_df["model"] = ["spatial"]*len(spatial_df)
+    spatial_df["model"] = ["spatial"] * len(spatial_df)
 
     # load temporal data
-    # temporal_df = pd.read_csv(sys.argv[2])
-    # temporal_df["model"] = ["temporal"] * len(temporal_df)
+    temporal_df = pd.read_csv(sys.argv[2])
+    temporal_df["model"] = ["temporal"] * len(temporal_df)
 
-    #df = spatial_df.append(temporal_df)
-    df = spatial_df
+    df = spatial_df.append(temporal_df)
     df["correct"] = df["expected_action"] == df["observed_action"]
 
     # group data by label
@@ -22,5 +21,7 @@ if __name__ == "__main__":
     print(df.head(10))
 
     # generate graph of accuracy
+    df.plt.hist(["correct"])
+    plt.show()
 
     # save plt
