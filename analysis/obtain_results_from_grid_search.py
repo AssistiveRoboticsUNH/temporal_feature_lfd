39f6fe063d4cc20161f10fe6afaf1e4a4f80583f
mbc2004 <mbc2004@cs.unh.edu>
2020-09-10 11:30:54 -0400

    debug obtain_results_from_grid_search.py

--- analysis/obtain_results_from_grid_search.py
+++ analysis/obtain_results_from_grid_search.py
@@ -7,14 +7,18 @@ import sys
 if __name__ == "__main__":
     # load spatial data
     spatial_df = pd.read_csv(sys.argv[1])
+    spatial_df["model"] = ["spatial"]*len(spatial_df)
 
     # load temporal data
-    temporal_df = pd.read_csv(sys.argv[2])
+    # temporal_df = pd.read_csv(sys.argv[2])
+    # temporal_df["model"] = ["temporal"] * len(temporal_df)
 
-    # add labels (bottleneck size, and temporal v. spatial information)
+    #df = spatial_df.append(temporal_df)
+    df = spatial_df
+    df["correct"] = df["expected_action"] == df["observed_action"]
 
     # group data by label
-    # group_df = df.group_by("")
+    df = df.group_by(["model", "bottleneck"])
 
     # generate graph of accuracy
 
