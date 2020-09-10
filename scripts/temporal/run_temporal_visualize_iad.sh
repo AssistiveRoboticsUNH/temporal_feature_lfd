60c39d29768e39837204f7a027bf152dd5c7ff5d
mbc2004 <mbc2004@cs.unh.edu>
2020-09-10 13:47:40 -0400

    Visualize Bottleneck IAD

--- scripts/temporal/run_temporal_visualize_iad.sh
+++ scripts/temporal/run_temporal_visualize_iad.sh
@@ -2,6 +2,7 @@
 
 SEGMENTS=16
 BATCH=3
+BOTTLENECK=4
 
 SAVE_ID="model0"
 SAVE_ID_DITRL=$SAVE_ID"_ditrl"
@@ -9,7 +10,7 @@ SAVE_ID_DITRL=$SAVE_ID"_ditrl"
 BACKBONE_MODEL="saved_models/saved_model_"$SAVE_ID".backbone.pt"
 OUTPUT_NAME_DITRL="csv_output/output_"$SAVE_ID_DITRL".csv"
 
-VIZUALIZE_IAD="python3 iad_visualization.py bs --trim --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --backbone_modelname "$BACKBONE_MODEL" --num_segments "$SEGMENTS
+VIZUALIZE_IAD="python3 iad_visualization.py bs --trim --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --backbone_modelname "$BACKBONE_MODEL" --num_segments "$SEGMENTS" --bottleneck "$BOTTLENECK
 
 echo $VIZUALIZE_IAD
 
