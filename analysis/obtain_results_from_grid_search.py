401b6a039b29a507f359e0ff18e61099f96c60a1
mbc2004 <mbc2004@cs.unh.edu>
2020-09-10 13:45:50 -0400

    debug obtain_results_from_grid_search.py

--- analysis/obtain_results_from_grid_search.py
+++ analysis/obtain_results_from_grid_search.py
@@ -8,21 +8,32 @@ if __name__ == "__main__":
     # load spatial data
     spatial_df = pd.read_csv(sys.argv[1])
     spatial_df["model"] = ["spatial"] * len(spatial_df)
+    spatial_df["correct"] = spatial_df["expected_action"] == spatial_df["observed_action"]
+    spatial_df = spatial_df.groupby(["bottleneck"]).mean().reset_index()
 
     # load temporal data
     temporal_df = pd.read_csv(sys.argv[2])
     temporal_df["model"] = ["temporal"] * len(temporal_df)
+    temporal_df["correct"] = temporal_df["expected_action"] == temporal_df["observed_action"]
+    temporal_df = temporal_df.groupby(["bottleneck"]).mean().reset_index()
 
-    df = spatial_df.append(temporal_df)
-    df["correct"] = df["expected_action"] == df["observed_action"]
+    # df = spatial_df.append(temporal_df)
+    labels = spatial_df["bottleneck"].to_numpy()
+    spatial_data = spatial_df["correct"].to_numpy()
+    temporal_data = temporal_df["correct"].to_numpy()
+    df = pd.DataFrame({"spatial": spatial_data,
+                       "temporal": temporal_data}, index=labels)
 
-    # group data by label
-    df = df.groupby(["model", "bottleneck"]).mean()
-    print(df)
+    df.plot.bar()
+    plt.ylim(0, 1.0)
+    plt.ylabel("Accuracy")
+    plt.xlabel("Bottleneck Size")
 
-    # generate graph of accuracy
-    df.plot.hist(["correct"])
-    plt.savefig("analysis/fig/accuracy.png")
-    # plt.show()
+    plt.tight_layout()
 
     # save plt
+    # plt.show()
+    plt.savefig("analysis/fig/accuracy.png")
+
+
+
