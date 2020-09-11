29e9187d817e1d81912afe647a115c432f0f0bfd
mbc2004 <mbc2004@cs.unh.edu>
2020-09-11 10:13:09 -0400

    debug iad visualize

--- iad_visualization.py
+++ iad_visualization.py
@@ -20,7 +20,7 @@ def sparse_map_to_img(sparse_map, length):
     iad = np.zeros((num_features, length))
     print("iad shape:", iad.shape)
     for i, f in enumerate(sparse_map):
-        print(i, f)
+        print(i, f, iad[f].shape)
         for pair in f:
             iad[f, pair[0]:pair[1]] = 1
     iad *= -1
@@ -113,97 +113,3 @@ if __name__ == '__main__':
                                          train_model=False)
 
     run(lfd_params_obj, model_obj)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import argparse
-
-
-def visualize_iad(model, filename, num_segments=8, image_tmpl='image_{:05d}.jpg', output_filename="image_stitch.png"):
-
-    total_num_frames = len(os.listdir(filename))
-    print("total num frames:", total_num_frames)
-    print("num segments:", num_segments)
-
-    # collect frames
-    images = []
-    idxs = np.linspace(0, max(1, total_num_frames-1), num=num_segments, dtype=int)+1
-
-    for idx in idxs:
-        images.append(Image.open(os.path.join(filename, image_tmpl.format(idx))).convert('RGB'))
-
-    # stitch frames together
-    def get_concat_h(im1, im2):
-        dst = Image.new('RGB', (im1.width + im2.width, im1.height))
-        dst.paste(im1, (0, 0))
-        dst.paste(im2, (im1.width, 0))
-        return dst
-
-    def get_concat_v(im1, im2):
-        dst = Image.new('RGB', (im1.width, im1.height + im2.height))
-        dst.paste(im1, (0, 0))
-        dst.paste(im2, (0, im1.height))
-        return dst
-
-    model.rgb_net()
-
-
-
-    img = images[0]
-    for i in range(1, len(images)):
-        img = get_concat_h(img, images[i])
-
-    # save to file
-    img.save(os.path.join(args.fig_dir, output_filename))
-
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Generate IADs from input files')
-    parser.add_argument('input_file', help='the checkpoint file to use with the model')
-    parser.add_argument('--fig_dir', default="analysis/fig",help='the checkpoint file to use with the model')
-    parser.add_argument('--num_segments', default=8, type=int,help='the checkpoint file to use with the model')
-    parser.add_argument('--mode', default="train", choices=["train", "eval"],
-                        help='the checkpoint file to use with the model')
-
-    args = parser.parse_args()
-
-    outname = args.input_file.split("/")[-1]
-    print("outname:", outname)
-
-    num_segments=16
-
-    from parameter_parser import default_model_args
-    lfd_params_obj = default_model_args(trim=True,
-                                        use_ditrl=True,
-                                        save_id="model0_train",
-                                        backbone_model="saved_models/saved_model_model0.backbone.pt",
-                                        num_segments=num_segments)
-
-    from model.temporal_feature_extractor import TemporalFeatureExtractor
-    model_obj = TemporalFeatureExtractor(lfd_params_obj, use_pipeline=True, train_pipeline=False, use_model=False,
-                                         train_model=False)
-
-    visualize_iad(model_obj, args.input_file, output_filename=outname, num_segments=num_segments)
-
-    # read_file(args, args.input_file, mode="train", image_tmpl='image_{:05d}.jpg', output_filename="image_train_"+outname+".png")
-    # read_file(args, args.input_file, mode="eval", image_tmpl='image_{:05d}.jpg', output_filename="image_eval_"+outname+".png")
