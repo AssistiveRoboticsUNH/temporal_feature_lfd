061aa66407e49a11483af9815f72fa6052f13886
mbc2004 <mbc2004@cs.unh.edu>
2020-09-11 08:51:05 -0400

    add argparse parameters to obtain_results_from_grid_search.py

--- analysis/obtain_results_from_grid_search.py
+++ analysis/obtain_results_from_grid_search.py
@@ -1,18 +1,25 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+import os
+import argparse
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate IADs from input files')
+    parser.add_argument('spatial_results_file', help='the checkpoint file to use with the model')
+    parser.add_argument('temporal_results_file', default="analysis/fig", help='the checkpoint file to use with the model')
+    parser.add_argument('--output_file', default="accuracy.png", help='the checkpoint file to use with the model')
+    args = parser.parse_args()
+
     # load spatial data
-    spatial_df = pd.read_csv(sys.argv[1])
+    spatial_df = pd.read_csv(args.spatial_results_file)
     spatial_df["model"] = ["spatial"] * len(spatial_df)
     spatial_df["correct"] = spatial_df["expected_action"] == spatial_df["observed_action"]
     spatial_df = spatial_df.groupby(["bottleneck"]).mean().reset_index()
 
     # load temporal data
-    temporal_df = pd.read_csv(sys.argv[2])
+    temporal_df = pd.read_csv(args.temporal_results_file)
     temporal_df["model"] = ["temporal"] * len(temporal_df)
     temporal_df["correct"] = temporal_df["expected_action"] == temporal_df["observed_action"]
     temporal_df = temporal_df.groupby(["bottleneck"]).mean().reset_index()
@@ -32,8 +39,8 @@ if __name__ == "__main__":
     plt.tight_layout()
 
     # save plt
-    plt.show()
-    # plt.savefig("analysis/fig/accuracy.png")
+    #plt.show()
+    plt.savefig(os.path.join("analysis/fig/", args.output_file))
 
 
 
