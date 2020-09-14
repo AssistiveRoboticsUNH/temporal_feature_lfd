d064de9d4ac4806453c7e10939a31c946c198f1c
mbc2004 <mbc2004@cs.unh.edu>
2020-09-14 13:40:10 -0400

    update grid analysis files

--- analysis/obtain_results_from_grid_search_spatial_only.py
+++ analysis/obtain_results_from_grid_search_spatial_only.py
@@ -6,12 +6,11 @@ import argparse
 
 
 if __name__ == "__main__":
-    target = "bottleneck"
+    target = "gaussian_value"
 
     parser = argparse.ArgumentParser(description='Generate IADs from input files')
     parser.add_argument('spatial_results_file', help='the checkpoint file to use with the model')
-    parser.add_argument('temporal_results_file', default="analysis/fig", help='the checkpoint file to use with the model')
-    parser.add_argument('--output_file', default="accuracy.png", help='the checkpoint file to use with the model')
+    parser.add_argument('--output_file', default="gaussian_accuracy.png", help='the checkpoint file to use with the model')
     args = parser.parse_args()
 
     # load spatial data
@@ -20,18 +19,10 @@ if __name__ == "__main__":
     spatial_df["correct"] = spatial_df["expected_action"] == spatial_df["observed_action"]
     spatial_df = spatial_df.groupby([target]).mean().reset_index()
 
-    # load temporal data
-    temporal_df = pd.read_csv(args.temporal_results_file)
-    temporal_df["model"] = ["temporal"] * len(temporal_df)
-    temporal_df["correct"] = temporal_df["expected_action"] == temporal_df["observed_action"]
-    temporal_df = temporal_df.groupby([target]).mean().reset_index()
-
     # df = spatial_df.append(temporal_df)
     labels = spatial_df[target].to_numpy()
     spatial_data = spatial_df["correct"].to_numpy()
-    temporal_data = temporal_df["correct"].to_numpy()
-    df = pd.DataFrame({"spatial": spatial_data,
-                       "temporal": temporal_data}, index=labels)
+    df = pd.DataFrame({"spatial": spatial_data}, index=labels)
 
     df.plot.bar()
     plt.ylim(0, 1.0)
