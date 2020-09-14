71a9e6eafc48472b0bf2bdac74b3480b1fc4dcec
mbc2004 <mbc2004@cs.unh.edu>
2020-09-14 13:45:55 -0400

    update grid analysis files

--- grid_spatial.py
+++ grid_spatial.py
@@ -15,38 +15,42 @@ if __name__ == '__main__':
 
     lfd_params = parse_model_args()
     output_df = pd.DataFrame()
+    num_repeats = 5
 
     #for bottleneck_size in [128, 64, 32, 16, 8, 4]:
     for gaussian_value in [0, 1, 2, 3, 4]:
+        for r in range(num_repeats):
 
-        # parameter changes
-        lfd_params.args.bottleneck = 4#bottleneck_size
-        lfd_params.args.gaussian_value = gaussian_value
-        lfd_params.args.save_id = "grid_gauss_"+str(gaussian_value)
-        lfd_params.locate_model_files_from_save_id()
+            # parameter changes
+            lfd_params.args.bottleneck = 4#bottleneck_size
+            lfd_params.args.gaussian_value = gaussian_value
+            lfd_params.args.save_id = "grid_gauss_"+str(gaussian_value)
+            lfd_params.locate_model_files_from_save_id()
 
-        # train model
-        print(" --- ")
-        print("Begin Training with value: {0}".format(gaussian_value))
-        print(" --- ")
-        model_obj = SpatialFeatureExtractor(lfd_params, is_training=True)
-        train(lfd_params, model_obj, debug=False)
-        print(" --- ")
-        print("Finished Training with value: {0}".format(gaussian_value))
-        print(" --- ")
+            # train model
+            print(" --- ")
+            print("Begin Training with value: {0}".format(gaussian_value))
+            print(" --- ")
+            model_obj = SpatialFeatureExtractor(lfd_params, is_training=True)
+            train(lfd_params, model_obj, debug=False)
+            print(" --- ")
+            print("Finished Training with value: {0}".format(gaussian_value))
+            print(" --- ")
 
-        # evaluate model
-        print(" --- ")
-        print("Begin Evaluating with value: {0}".format(gaussian_value))
-        print(" === ")
-        lfd_params.locate_model_files_from_save_id()
-        model_obj = SpatialFeatureExtractor(lfd_params, is_training=False)
-        df = evaluate(lfd_params, model_obj, debug=False)
-        df["gaussian_value"] = np.array([gaussian_value]*len(df))
-        output_df = output_df.append(df)
-        print(" --- ")
-        print("Finished Evaluating with value: {0}".format(gaussian_value))
-        print(" === ")
+            # evaluate model
+            print(" --- ")
+            print("Begin Evaluating with value: {0}".format(gaussian_value))
+            print(" === ")
+            lfd_params.locate_model_files_from_save_id()
+            model_obj = SpatialFeatureExtractor(lfd_params, is_training=False)
+            df = evaluate(lfd_params, model_obj, debug=False)
+
+            df["gaussian_value"] = np.array([gaussian_value]*len(df))
+            df["repeat"] = np.array([r]*len(df))
+            output_df = output_df.append(df)
+            print(" --- ")
+            print("Finished Evaluating with value: {0}".format(gaussian_value))
+            print(" === ")
 
     # analyze output of spatial
     out_filename = os.path.join(lfd_params.args.output_dir, "output_grid_spatial_gaussian.csv")
