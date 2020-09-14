0a0562e50e95340103e1491a0158289e37780d57
mbc2004 <mbc2004@cs.unh.edu>
2020-09-14 08:56:57 -0400

    video pre-processing

--- scripts/spatial/run_spatial.sh
+++ scripts/spatial/run_spatial.sh
@@ -4,12 +4,13 @@ SEGMENTS=16
 EPOCHS=200
 ALPHA=0.0005
 BATCH=3
+BOTTLENECK=4
 
 SAVE_ID="model0"
 OUTPUT_NAME="csv_output/output_"$SAVE_ID".csv"
 
-TRAIN_CMD="python3 train_spatial.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA
-EVAL_CMD="python3 eval_spatial.py bs --trim --save_id "$SAVE_ID
+TRAIN_CMD="python3 train_spatial.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA" --bottleneck "$BOTTLENECK
+EVAL_CMD="python3 eval_spatial.py bs --trim --save_id "$SAVE_ID" --bottleneck "$BOTTLENECK
 ANALYZE_CMD="python3 analysis/action_metrics.py "$OUTPUT_NAME
 
 echo $TRAIN_CMD
