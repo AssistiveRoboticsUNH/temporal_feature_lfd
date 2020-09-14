00c9888f8e476e72b813391658a82ad3ece3d14c
mbc2004 <mbc2004@cs.unh.edu>
2020-09-14 09:38:03 -0400

    view gaussian modifications

--- analysis/image_capture.py
+++ analysis/image_capture.py
@@ -1,5 +1,5 @@
-import sys, os
-from PIL import Image
+import os
+from PIL import Image, ImageFilter
 import numpy as np 
 
 import argparse
@@ -20,7 +20,7 @@ def get_concat_v(im1, im2):
 
 
 def read_file(num_segments, input_file, mode="train", image_tmpl='image_{:05d}.jpg', output_filename="image_stitch.png",
-              save_file=True, merge_images=True):
+              save_file=True, merge_images=True, gaussian_value=0):
 
     total_num_frames = len(os.listdir(input_file))
     print("total num frames:", total_num_frames)
@@ -34,7 +34,9 @@ def read_file(num_segments, input_file, mode="train", image_tmpl='image_{:05d}.j
         idxs = np.linspace(0, max(1, total_num_frames-1), num=num_segments*10, dtype=int)+1
 
     for idx in idxs:
-        images.append(Image.open(os.path.join(input_file, image_tmpl.format(idx))).convert('RGB'))
+        img = Image.open(os.path.join(input_file, image_tmpl.format(idx))).convert('RGB')
+        img = img.filter(ImageFilter.GaussianBlur(gaussian_value))
+        images.append(img)
 
     # stitch frames together
     if merge_images:
@@ -57,6 +59,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_segments', default=8, type=int,help='the checkpoint file to use with the model')
     parser.add_argument('--mode', default="train", choices=["train", "eval"],
                         help='the checkpoint file to use with the model')
+    parser.add_argument('--gaussian_value', default=0, type=int, help='the checkpoint file to use with the model')
 
     args = parser.parse_args()
 
