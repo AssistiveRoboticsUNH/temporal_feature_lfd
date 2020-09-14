eddf4fc2e179e80e3bdd66a8a61374daf0c92e35
mbc2004 <mbc2004@cs.unh.edu>
2020-09-14 14:38:56 -0400

    update grid analysis files

--- analysis/obtain_results_from_grid_search_spatial_only.py
+++ analysis/obtain_results_from_grid_search_spatial_only.py
@@ -18,13 +18,22 @@ if __name__ == "__main__":
     spatial_df["model"] = ["spatial"] * len(spatial_df)
     spatial_df["correct"] = spatial_df["expected_action"] == spatial_df["observed_action"]
     spatial_df = spatial_df.groupby([target, "repeat"]).mean().reset_index()
+
     spatial_df_mean = spatial_df.groupby([target]).mean().reset_index()
     spatial_df_std = spatial_df.groupby([target]).std().reset_index()
 
     # df = spatial_df.append(temporal_df)
-    labels = spatial_df[target].to_numpy()
-    spatial_data = spatial_df["correct"].to_numpy()
-    df = pd.DataFrame({"spatial": spatial_df_mean, 'std': spatial_df_std}, index=labels)
+    labels = spatial_df[target].unique()
+    spatial_data_mean = spatial_df_mean["correct"].to_numpy()
+    spatial_data_std = spatial_df_std["correct"].to_numpy()
+
+    print(labels)
+    print(spatial_data_mean)
+    print(spatial_data_std)
+
+    df = pd.DataFrame({"spatial": spatial_data_mean, 'std': spatial_data_std}, index=labels)
+
+    print(df)
 
     df.plot.bar()
     plt.ylim(0, 1.0)
