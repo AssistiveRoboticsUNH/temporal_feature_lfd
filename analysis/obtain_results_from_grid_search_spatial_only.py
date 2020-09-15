5f07cc84f187a84760b3b69a05f0f6aab9aca67a
mbc2004 <mbc2004@cs.unh.edu>
2020-09-15 10:25:02 -0400

    updated spatial feature analysis

--- analysis/obtain_results_from_grid_search_spatial_only.py
+++ analysis/obtain_results_from_grid_search_spatial_only.py
@@ -1,20 +1,10 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import os
 import argparse
 
 
-if __name__ == "__main__":
-    target = "gaussian_value"
-
-    parser = argparse.ArgumentParser(description='Generate IADs from input files')
-    parser.add_argument('spatial_results_file', help='the checkpoint file to use with the model')
-    parser.add_argument('--output_file', default="gaussian_accuracy.png", help='the checkpoint file to use with the model')
-    args = parser.parse_args()
-
-    # load spatial data
-    spatial_df = pd.read_csv(args.spatial_results_file)
+def accuracy(spatial_df, output_filename, target, title=""):
     spatial_df["model"] = ["spatial"] * len(spatial_df)
     spatial_df["correct"] = spatial_df["expected_action"] == spatial_df["observed_action"]
     spatial_df = spatial_df.groupby([target, "repeat"]).mean().reset_index()
@@ -22,7 +12,6 @@ if __name__ == "__main__":
     spatial_df_mean = spatial_df.groupby([target]).mean().reset_index()
     spatial_df_std = spatial_df.groupby([target]).std().reset_index()
 
-    # df = spatial_df.append(temporal_df)
     labels = spatial_df[target].unique()
     spatial_data_mean = spatial_df_mean["correct"].to_numpy()
     spatial_data_std = spatial_df_std["correct"].to_numpy()
@@ -35,18 +24,67 @@ if __name__ == "__main__":
 
     print(df)
 
-    df.plot.bar()
+    df.plot.bar(yerr=spatial_data_std, ecolor='black', capsize=5)
     plt.ylim(0, 1.0)
     plt.ylabel("Accuracy")
     plt.xlabel(target)
 
-    plt.errorbar(df.index, df['spatial'], yerr=spatial_data_std)
-
+    plt.title(title)
     plt.tight_layout()
 
     # save plt
-    #plt.show()
-    plt.savefig(os.path.join("analysis/fig/", args.output_file))
+    # plt.show()
+    plt.savefig(output_filename)
+
+
+def breakdown(spatial_df, output_filename, target, title=""):
+    spatial_df["model"] = ["spatial"] * len(spatial_df)
+    spatial_df["correct"] = spatial_df["expected_action"] == spatial_df["observed_action"]
+    spatial_df = spatial_df.groupby([target, "repeat", "expected_action"]).mean().reset_index()
+
+    spatial_df_mean = spatial_df.groupby([target, "expected_action"]).mean().reset_index()
+    # spatial_df_std = spatial_df.groupby([target, "expected_action"]).std()#.reset_index()
+
+    label_order = [1,3,4,2,0,5,6]
+    label_dict = {0: 'r', 1: 'g', 2: 'b', 3: 'gb', 4: 'bg', 5: 'rr', 6: 'rrr'}  # matches labels in block construction
+    colors = {"r": "r", "g": "g", "b": "b", "bg": "cyan", "gb": "springgreen", "rr": "indianred", "rrr": "brown"}
+    columns = {}
+    for k in label_order:
+        columns[label_dict[k]] = spatial_df_mean.loc[spatial_df_mean['expected_action'] == k]["correct"].to_numpy()
+
+    labels = spatial_df[target].unique()
+    df = pd.DataFrame(columns, index=labels)
+
+    df.plot.bar(color=colors)
+    plt.ylim(0, 1.0)
+    plt.ylabel("Accuracy")
+    plt.xlabel(target)
+
+    plt.title(title)
+    plt.tight_layout()
+
+    # save plt
+    plt.show()
+    plt.savefig(output_filename)
+
+
+if __name__ == "__main__":
+    target_label = "gaussian_value"
+
+    parser = argparse.ArgumentParser(description='Generate IADs from input files')
+    parser.add_argument('spatial_results_file', help='the checkpoint file to use with the model')
+    parser.add_argument('--output_file', default="", help='the checkpoint file to use with the model')
+    args = parser.parse_args()
+
+    # load spatial data
+    spatial_df_src = pd.read_csv(args.spatial_results_file)
+
+    # run analysis
+    output_filename_acc = os.path.join("analysis/fig/", args.output_file + "accuracy.png")
+    accuracy(spatial_df_src, output_filename_acc, target_label, title=target_label+" Accuracy")
+
+    output_filename_bd = os.path.join("analysis/fig/", args.output_file + "breakdown.png")
+    breakdown(spatial_df_src, output_filename_bd, target_label, title=target_label+" Breakdown")
 
 
 
