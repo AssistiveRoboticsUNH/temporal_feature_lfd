1a44204a39d4caa09bedd34275c2966ddf264c7f
mbc2004 <mbc2004@cs.unh.edu>
2020-09-15 16:13:36 -0400

    masking: image difference

--- analysis/image_capture.py
+++ analysis/image_capture.py
@@ -103,11 +103,12 @@ def applySaliencyMap(img_array):
     image_out = []
     for img in img_array:
         gray = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
-        print(gray)
+        print("0:", gray)
         (success, saliencyMap) = saliency.computeSaliency(gray)
         print("success:", success)
+        print("1:", saliencyMap)
         saliencyMap = (saliencyMap * 255).astype("uint8")
-        print(saliencyMap)
+        print("2:", saliencyMap)
         saliencyMap = Image.fromarray(saliencyMap)
 
         image_out.append(saliencyMap)
