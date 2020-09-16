8ca888559547c310300cacafef297946342bc8b9
mbc2004 <mbc2004@cs.unh.edu>
2020-09-15 23:29:48 -0400

    masking: saliency map

--- analysis/image_capture.py
+++ analysis/image_capture.py
@@ -68,7 +68,7 @@ def applyDifferenceMask(img_array):
         frame = np.array(img)
         fgMask = backSub.apply(frame)
         fgMask = cv2.cvtColor(fgMask, cv2.COLOR_GRAY2BGR)
-        fgMask = Image.fromarray(fgMask * frame)
+        fgMask = Image.fromarray((fgMask * frame).astype(np.uint8))
         image_out.append(fgMask)
 
     return image_out
