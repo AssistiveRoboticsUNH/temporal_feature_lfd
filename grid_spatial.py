fa1f60712deac32154d1421fadc240193aca374c
mbc2004 <mbc2004@cs.unh.edu>
2020-09-17 09:49:10 -0400

    debug spatial features

--- grid_spatial.py
+++ grid_spatial.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     num_repeats = 5
 
     value_name = "bottleneck"
-    for value in [128, 64, 32, 16, 8, 4]:
+    for value in [4]:#[128, 64, 32, 16, 8, 4]:
         for r in range(1):
 
             # parameter changes
