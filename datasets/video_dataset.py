eab45806fc10b0aa6ebf62a56e1d393afe05ee9c
mbc2004 <mbc2004@cs.unh.edu>
2020-09-18 10:14:31 -0400

    long training

--- datasets/video_dataset.py
+++ datasets/video_dataset.py
@@ -130,11 +130,11 @@ class VideoDataset(Dataset):
         self.obs_dict = {}
 
         for obs in os.listdir(root_path):
-            if obs in ['r', 'g', 'b']:
-                all_obs_files = os.listdir(os.path.join(root_path, obs))
-                self.obs_dict[obs] = all_obs_files
-                self.data.extend(all_obs_files)
-                print("obs:", obs)
+            #if obs in ['r', 'g', 'b']:
+            all_obs_files = os.listdir(os.path.join(root_path, obs))
+            self.obs_dict[obs] = all_obs_files
+            self.data.extend(all_obs_files)
+            #     print("obs:", obs)
 
         # how to transform the images
         input_size = 224
