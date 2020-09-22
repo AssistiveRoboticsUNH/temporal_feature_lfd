f65224b6d5764e4377753decb14e9c6eba6cbe2a
mbc2004 <mbc2004@cs.unh.edu>
2020-09-22 08:55:22 -0400

    addition of simpleNet

--- model/backbone_model/simplenet.py
+++ model/backbone_model/simplenet.py
@@ -20,10 +20,12 @@ class SimpleNet(nn.Module):
         self.bottleneck_size = bottleneck_size
         self.pretrained_checkpoint = pretrained_checkpoint
 
+        hidden_layer = 4
+
         self.model = nn.Sequential(
-            nn.Conv2d(3, 8, 3),
+            nn.Conv2d(3, hidden_layer, 3),
             nn.ReLU(),
-            nn.Conv2d(8, self.bottleneck_size, (1, 1)),
+            nn.Conv2d(hidden_layer, self.bottleneck_size, (1, 1)),
             nn.AdaptiveMaxPool2d(output_size=1),
             )
 
