867aa729a652abe391c6dd55542af8ea2327c9f9
mbc2004 <mbc2004@cs.unh.edu>
2020-09-22 09:35:56 -0400

    addition of simpleNet

--- model/spatial_feature_extractor.py
+++ model/spatial_feature_extractor.py
@@ -46,10 +46,10 @@ class SpatialFeatureExtractor(FeatureExtractor):
 		# consensus module
 
 		rgb_y = rgb_y.view((-1, self.rgb_net.num_segments) + rgb_y.size()[1:])
-
+		print("rgb_y 1:", rgb_y)
 		rgb_y = self.consensus(rgb_y)
 		rgb_y = rgb_y.squeeze(1)
-		print("rgb_y:", rgb_y)
+		print("rgb_y 2:", rgb_y)
 		obs_y = self.linear(rgb_y)
 
 		"""
