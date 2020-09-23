c6ea74525ba69d576dd7fdd6dbc51c445e152250
mbc2004 <mbc2004@cs.unh.edu>
2020-09-23 11:25:02 -0400

    vizualize generated data

--- view_dataset.py
+++ view_dataset.py
@@ -76,10 +76,12 @@ if __name__ == '__main__':
 
     img_dict = {}
 
+    root_path = os.path.join("/home/mbc2004/", "datasets/BlockConstruction/frames/", "train")
+    print("root_path:", root_path)
+
     for mode in ["train", "evaluation"]:
 
-        root_path = os.path.join("/home/mbc2004/", "datasets/BlockConstruction/frames/", mode)
-        print("root_path:", root_path)
+
 
         vd = VideoDataset(root_path, mode, full_sample, image_tmpl=image_tmpl, num_segments=num_segments)
         if (mode == "train"):
