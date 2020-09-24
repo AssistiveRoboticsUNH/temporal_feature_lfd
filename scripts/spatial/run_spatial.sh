9679436c5e11ccf17a8bafe8199e956cb82e9afb
mbc2004 <mbc2004@cs.unh.edu>
2020-09-24 01:45:45 -0400

    get to overfit: use full dataset

--- scripts/spatial/run_spatial.sh
+++ scripts/spatial/run_spatial.sh
@@ -7,7 +7,7 @@ BATCH=4
 BOTTLENECK=16
 GAUSS=1
 
-SAVE_ID="regular_tsm"
+SAVE_ID="regular_tsm_full_bs"
 OUTPUT_NAME="csv_output/output_"$SAVE_ID".csv"
 
 TRAIN_CMD="python3 train_spatial.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA" --bottleneck "$BOTTLENECK" --gaussian_value "$GAUSS
