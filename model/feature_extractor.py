e1251507ced2725f61d2ef2d8a7d3eceb625f5ce
mbc2004 <mbc2004@cs.unh.edu>
2020-09-24 09:18:28 -0400

    get temporal model running

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -27,12 +27,6 @@ class FeatureExtractor(nn.Module):
 			self.checkpoint_file = lfd_params.args.backbone_modelname
 			pretrained_checkpoint = False
 
-		print("self.checkpoint_file :", self.checkpoint_file)
-		print("lfd_params.args.pretrain_modelname :", lfd_params.args.pretrain_modelname)
-		print("fd_params.args.backbone_modelname :", lfd_params.args.backbone_modelname)
-
-		# rgb net
-
 		# TSM
 		from .backbone_model.tsm.tsm import TSMWrapper as VisualFeatureExtractor
 
