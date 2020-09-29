01bd19d3a795a47fe22673446b9b05d1e374be50
mbc2004 <mbc2004@cs.unh.edu>
2020-09-29 14:32:07 -0400

    RGB only, simple model, bn 16, 3 classes, spatial

--- scripts/spatial/run_spatial.sh
+++ scripts/spatial/run_spatial.sh
@@ -7,7 +7,7 @@ BATCH=4
 BOTTLENECK=16
 GAUSS=1
 
-SAVE_ID="simple_rgb_bs_bn16_run3"
+SAVE_ID="simple_rgb_bs_bn16_run4"
 OUTPUT_NAME="csv_output/output_"$SAVE_ID".csv"
 
 TRAIN_CMD="python3 train_spatial.py bs --trim --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID" --num_segments "$SEGMENTS" --lr "$ALPHA" --bottleneck "$BOTTLENECK" --gaussian_value "$GAUSS
