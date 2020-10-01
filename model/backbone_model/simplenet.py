6a07b2077b66c9084c58456ff1ec5e1b62bfda55
mbc2004 <mbc2004@cs.unh.edu>
2020-09-30 22:04:57 -0400

    RGB only, simple model, bn 1, 2 classes

--- model/backbone_model/simplenet.py
+++ model/backbone_model/simplenet.py
@@ -44,8 +44,10 @@ class SimpleNet(nn.Module):
         self.model.load_state_dict(new_state_dict, strict=not training)
 
     def forward(self, inp):
+        print("inp.shape:", inp.shape)
         sample_len = 3
         inp = inp.view((-1, sample_len) + inp.size()[-2:])
+        print("inp.shape 2:", inp.shape)
 
         # print("inp.shape:", inp.shape)
         # m = inp.max(2)[0].max(2)[0]
