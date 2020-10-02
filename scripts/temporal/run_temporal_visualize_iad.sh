cee8fa10113801015ded27b6e8e87d85963614d6
mbc2004 <mbc2004@cs.unh.edu>
2020-10-02 08:54:54 -0400

    iad visualization extract data points

--- scripts/temporal/run_temporal_visualize_iad.sh
+++ scripts/temporal/run_temporal_visualize_iad.sh
@@ -8,7 +8,7 @@ SAVE_ID="regular_tsm_full_bs_bn32"
 SAVE_ID_DITRL=$SAVE_ID #"_ditrl"
 
 BACKBONE_MODEL="saved_models/saved_model_"$SAVE_ID".backbone.pt"
-VIZUALIZE_IAD="python3 iad_visualization.py bs --trim --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --backbone_modelname "$BACKBONE_MODEL" --num_segments "$SEGMENTS" --bottleneck "$BOTTLENECK
+VIZUALIZE_IAD="python3 iad_visualization_analyze.py bs --trim --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --backbone_modelname "$BACKBONE_MODEL" --num_segments "$SEGMENTS" --bottleneck "$BOTTLENECK
 
 echo $VIZUALIZE_IAD
 
