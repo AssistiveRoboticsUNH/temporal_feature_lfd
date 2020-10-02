e419d59200485ba082d70bd25d183d0102528736
mbc2004 <mbc2004@cs.unh.edu>
2020-10-01 20:59:26 -0400

    RGB only, simple model, bn 1, 2 classes

--- iad_visualization.py
+++ iad_visualization.py
@@ -71,8 +71,11 @@ def run(lfd_params, model):
                 # is a challenge that will involve a lot of messy programming).
                 iad = model.pipeline.convert_activation_map_to_iad(activation_map[n])
 
-                print ("IAD")
-                print(iad)
+                print ("IADmin")
+                print(iad.min(axis=1))
+                print("IADmax")
+                print(iad.max(axis=1))
+                print("")
 
                 sparse_map = model.pipeline.convert_iad_to_sparse_map(iad)
                 iad_img = sparse_map_to_img(sparse_map, lfd_params.args.num_segments)
