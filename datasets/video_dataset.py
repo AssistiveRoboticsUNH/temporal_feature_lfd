96eee7659f3f4d426be3e22ead8204f6fda5b61c
mbc2004 <mbc2004@cs.unh.edu>
2020-10-06 15:42:43 -0400

    add save back in

--- datasets/video_dataset.py
+++ datasets/video_dataset.py
@@ -131,11 +131,11 @@ class VideoDataset(Dataset):
         self.get_filename = False
 
         for obs in os.listdir(root_path):
-            if obs in ['r', 'b', 'g']:
-                all_obs_files = os.listdir(os.path.join(root_path, obs))
-                self.obs_dict[obs] = all_obs_files
-                for obs_files in all_obs_files:
-                    self.data.append(os.path.join(*[root_path, obs, obs_files]))
+            #if obs in ['r', 'b', 'g']:
+            all_obs_files = os.listdir(os.path.join(root_path, obs))
+            self.obs_dict[obs] = all_obs_files
+            for obs_files in all_obs_files:
+                self.data.append(os.path.join(*[root_path, obs, obs_files]))
 
         # how to transform the images
         input_size = 224
