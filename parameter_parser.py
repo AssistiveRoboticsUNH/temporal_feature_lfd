6b1d500b142fa30d65c04a254c0dc0ea9d12be0d
mbc2004 <mbc2004@cs.unh.edu>
2020-10-06 10:21:09 -0400

    establish spatial baseline accuracy

--- parameter_parser.py
+++ parameter_parser.py
@@ -112,7 +112,7 @@ class Parameters:
     def setup_block_stacking(self):
         print("Loading block_construction_dl")
         self.file_directory = os.path.join(ROOT_DIR, "datasets/BlockConstruction/frames/")
-        self.num_actions = 3#7
+        self.num_actions = 7
         self.num_hidden_state_params = 1
 
         self.use_aud = False
