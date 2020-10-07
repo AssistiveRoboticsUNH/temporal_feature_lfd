364772f4c01177289cb641dc8e4bd6658fa4be73
mbc2004 <mbc2004@cs.unh.edu>
2020-10-07 10:31:04 -0400

    grid spatial

--- scripts/temporal/run_temporal.sh
+++ scripts/temporal/run_temporal.sh
@@ -6,7 +6,7 @@ ALPHA=0.0001
 BATCH=4
 BOTTLENECK=32
 
-SAVE_ID="regular_tsm_full_bs_bn32"
+SAVE_ID="regular_tsm_full_bs_bn"$BOTTLENECK
 SAVE_ID_DITRL=$SAVE_ID"_ditrl"
 
 BACKBONE_MODEL="saved_models/saved_model_"$SAVE_ID".backbone.pt"
