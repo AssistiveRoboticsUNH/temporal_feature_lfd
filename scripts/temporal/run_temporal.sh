66cdbe4f7f8605e1aafa9067ae1d2df29c6226f5
mbc2004 <mbc2004@cs.unh.edu>
2020-10-08 09:02:27 -0400

    train temporal

--- scripts/temporal/run_temporal.sh
+++ scripts/temporal/run_temporal.sh
@@ -4,9 +4,9 @@ SEGMENTS=16 #16
 EPOCHS=50
 ALPHA=0.0001
 BATCH=4
-BOTTLENECK=32
+BOTTLENECK=64
 
-SAVE_ID="regular_tsm_full_bs_bn"$BOTTLENECK
+SAVE_ID="tsm_bottleneck_"$BOTTLENECK
 SAVE_ID_DITRL=$SAVE_ID"_ditrl"
 
 BACKBONE_MODEL="saved_models/saved_model_"$SAVE_ID".backbone.pt"
