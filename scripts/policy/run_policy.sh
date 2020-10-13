eb6f36eabdb5d97268ced669d1044fdec5e8e7dd
mbc2004 <mbc2004@cs.unh.edu>
2020-10-13 11:31:33 -0400

    evaluate LSTM policy model

--- scripts/policy/run_policy.sh
+++ scripts/policy/run_policy.sh
@@ -14,14 +14,14 @@ OUTPUT_NAME_DITRL="csv_output/output_"$SAVE_ID_DITRL".csv"
 
 #GENERATE_ITR_CMD="python3 train_temporal_policy.py bs --trim --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --backbone_modelname "$BACKBONE_MODEL" --num_segments "$SEGMENTS" --bottleneck_size "$BOTTLENECK
 TRAIN_CMD="python3 train_temporal_policy.py bs --epochs "$EPOCHS" --batch_size "$BATCH" --save_id "$SAVE_ID_DITRL" --ditrl --lr "$ALPHA" --bottleneck_size "$BOTTLENECK
-#EVAL_CMD="python3 eval_temporal.py bs --trim --save_id "$SAVE_ID_DITRL" --ditrl --bottleneck_size "$BOTTLENECK
+EVAL_CMD="python3 eval_temporal_policy.py bs --trim --save_id "$SAVE_ID_DITRL" --ditrl --bottleneck_size "$BOTTLENECK
 #ANALYZE_CMD="python3 analysis/action_metrics.py "$OUTPUT_NAME_DITRL
 
 #echo $GENERATE_ITR_CMD
 echo ""
 echo $TRAIN_CMD
 echo ""
-#echo $EVAL_CMD
+echo $EVAL_CMD
 echo ""
 #echo $ANALYZE_CMD
 echo ""
@@ -37,15 +37,15 @@ echo "====="
 echo "TRAIN"
 echo "====="
 
-echo $TRAIN_CMD
-eval $TRAIN_CMD
+#echo $TRAIN_CMD
+#eval $TRAIN_CMD
 
 echo "====="
 #echo "EVAL"
 echo "====="
 
-#echo $EVAL_CMD
-#eval $EVAL_CMD
+echo $EVAL_CMD
+eval $EVAL_CMD
 
 echo "====="
 #echo "ANALYZE"
