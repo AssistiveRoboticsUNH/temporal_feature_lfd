7235e63c1760cb95daf0aded66dbe29cafd8d480
mbc2004 <mbc2004@cs.unh.edu>
2020-10-13 15:55:21 -0400

    evaluate LSTM policy model

--- datasets/generate_traces.py
+++ datasets/generate_traces.py
@@ -19,43 +19,43 @@ def gen_path2(length=10):
     while j < len(act):
         add = 1
 
-        print("s:", j)
+        #print("s:", j)
 
         if act[j] == 1:
-            print("c1")
+            #print("c1")
             obs[j] = 1
         if j < len(act)-1 and act[j] == 1 and act[j+1] == 1:
-            print("c2")
+           # print("c2")
             obs[j] = 2
             obs[j+1] = 0
             add = 2
         if j < len(act)-2 and act[j] == 1 and act[j+1] == 1 and act[j+2] == 1:
-            print("c3")
+          #  print("c3")
             obs[j] = 3
             obs[j + 1] = 0
             obs[j + 2] = 0
             add = 3
 
         if act[j] == 2:
-            print("c4")
+            #print("c4")
             obs[j] = 4
         if j < len(act)-1 and act[j] == 2 and act[j+1] == 3:
-            print("c5")
+            #print("c5")
             obs[j] = 5
             obs[j + 1] = 0
             add = 2
 
         if act[j] == 3:
-            print("c6")
+            #print("c6")
             obs[j] = 7
         if j < len(act)-1 and act[j] == 3 and act[j+1] == 2:
-            print("c7")
+            #print("c7")
             obs[j] = 6
             obs[j + 1] = 0
             add = 2
 
         j += add
-        print("e", j)
+        #print("e", j)
 
     return obs, act
 
