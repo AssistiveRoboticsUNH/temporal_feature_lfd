9f1d3fc0d1be66413453609b63f91320fa659d37
mbc2004 <mbc2004@cs.unh.edu>
2020-10-12 23:26:03 -0400

    prepare LSTM policy model

--- model/model.py
+++ model/model.py
@@ -64,7 +64,7 @@ class LfDNetwork(nn.Module):
         # combine visual features with empty action
         #state_x = state_x.type(torch.FloatTensor).view([-1, self.lfd_params.num_actions]).cuda()
         #action_x = Variable(torch.zeros(obs_y.shape[0], self.lfd_params.num_actions)).cuda()
-        state_x = torch.cat([obs_y, history], out=None)
+        state_x = torch.cat([obs_y, history], dim=1, out=None)
         print("T2:", state_x.shape)
 
         # combine input history with most recent observation
