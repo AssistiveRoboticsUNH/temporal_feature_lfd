4ed8844e64c91267451a6bfa0d3fe91274d34c07
mbc2004 <mbc2004@cs.unh.edu>
2020-10-13 12:43:32 -0400

    evaluate LSTM policy model

--- eval_temporal_policy.py
+++ eval_temporal_policy.py
@@ -57,6 +57,8 @@ def eval_model(lfd_params, model, mode="evaluation"):
             obs_file.append(obs_src)
 
     correct = action_expected == action_selected
+    print("action_expected:", action_expected)
+    print("action_selected:", action_selected)
     print("correct:", correct)
 
     print("Accuracy: ", np.sum(correct)/float(len(action_expected)))
