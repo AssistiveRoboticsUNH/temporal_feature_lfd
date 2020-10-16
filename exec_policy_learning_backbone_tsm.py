0bc52ae1c90cfec5928bddf32afb732444e57a85
mbc2004 <mbc2004@cs.unh.edu>
2020-10-16 14:33:12 -0400

    Code re-factor: establish distinction between classifier and policy learner

--- exec_policy_learning_backbone_tsm.py
+++ exec_policy_learning_backbone_tsm.py
@@ -1,6 +1,6 @@
 import os
 from parameter_parser import parse_model_args, default_model_args
-from run_classification import train, evaluate
+from run_policy_learning import train, evaluate_single_action
 from model.policylearner_backbone_tsm import PolicyLearnerBackboneTSM
 
 if __name__ == '__main__':
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     model = PolicyLearnerBackboneTSM(lfd_params, filename, spatial_train=True, policy_train=True)
 
     train(lfd_params, model)
-    df = evaluate(lfd_params, model)
+    df = evaluate_single_action(lfd_params, model)
 
     out_filename = os.path.join(lfd_params.args.output_dir, "output_" + lfd_params.args.save_id + ".csv")
     df.to_csv(out_filename)
