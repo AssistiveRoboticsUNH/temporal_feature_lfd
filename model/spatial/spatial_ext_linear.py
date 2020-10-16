19924b2b1cc5054d32d60409aacb8e42f93cc642
mbc2004 <mbc2004@cs.unh.edu>
2020-10-16 14:21:31 -0400

    Code re-factor: include num_segments in dataset

--- model/spatial/spatial_ext_linear.py
+++ model/spatial/spatial_ext_linear.py
@@ -41,13 +41,14 @@ class SpatialExtLinear(nn.Module):
         print("spatial x.shape1:", x.shape)
 
         x = x.view((-1, self.lfd_params.args.num_segments) + x.size()[1:])
+        print("spatial x.shape2:", x.shape)
         x, _ = x.max(dim=1, keepdim=True)  # max consensus
         x = x.squeeze(1)
-        print("spatial x.shape2:", x.shape)
+        print("spatial x.shape3:", x.shape)
 
         x = torch.reshape(x, (-1, self.input_size))
 
-        print("spatial x.shape3:", x.shape)
+        print("spatial x.shape4:", x.shape)
         return self.fc(x)
 
     def save_model(self, filename):
