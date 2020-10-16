e3b7b27b1426d0f923d7984578dfcd56dc74fb8d
mbc2004 <mbc2004@cs.unh.edu>
2020-10-16 12:54:53 -0400

    Code re-factor: correct imports

--- parameter_parser.py
+++ parameter_parser.py
@@ -98,7 +98,7 @@ class Parameters:
 
     def setup_social_greeting(self):
         print("Loading social_greeting_dl")
-        self.file_directory = os.path.join(ROOT_DIR, "datasets_old/SocialGreeting/frames/")
+        self.file_directory = os.path.join(ROOT_DIR, "datasets/SocialGreeting/frames/")
         self.num_actions = 3
         self.num_hidden_state_params = 1
 
@@ -111,7 +111,7 @@ class Parameters:
 
     def setup_block_stacking(self):
         print("Loading block_construction_dl")
-        self.file_directory = os.path.join(ROOT_DIR, "datasets_old/BlockConstruction/frames/")
+        self.file_directory = os.path.join(ROOT_DIR, "datasets/BlockConstruction/frames/")
         self.num_actions = 7
         self.num_observations = 7
         self.num_hidden_state_params = 1
