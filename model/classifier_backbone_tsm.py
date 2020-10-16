e409e1df1b05e158f15f9e059c41742954cfd390
mbc2004 <mbc2004@cs.unh.edu>
2020-10-16 14:29:57 -0400

    Code re-factor: establish distinction between classifier and policy learner

--- model/classifier_backbone_tsm.py
+++ model/classifier_backbone_tsm.py
@@ -2,13 +2,11 @@ import torch.nn as nn
 
 from .backbone_model.backbone_tsm import BackboneTSM
 from .spatial.spatial_ext_linear import SpatialExtLinear
-from .policy.policy_lstm import PolicyLSTM
 
 
-class PolicyLearnerBackboneTSM(nn.Module):
+class ClassifierBackboneTSM(nn.Module):
     def __init__(self, lfd_params, filename,
-                 spatial_train=False,
-                 policy_train=False):
+                 spatial_train=False):
         super().__init__()
         self.lfd_params = lfd_params
 
@@ -17,22 +15,16 @@ class PolicyLearnerBackboneTSM(nn.Module):
 
         self.backbone_filename = ".".join([self.filename, "backbone", "pt"])
         self.spatial_filename = ".".join([self.filename, "spatial", "pt"])
-        self.lstm_filename = ".".join([self.filename, "lstm", "pt"])
-        self.fc_filename = ".".join([self.filename, "policy", "pt"])
 
         # model sections
         self.backbone = BackboneTSM(lfd_params, is_training=spatial_train,
                                     filename=lfd_params.args.pretrain_modelname if spatial_train else self.backbone)
         self.spatial = SpatialExtLinear(lfd_params, is_training=spatial_train, filename=self.spatial_filename,
                                         input_size=2048)
-        self.policy = PolicyLSTM(lfd_params, is_training=policy_train,
-                                 lstm_filename=self.lstm_filename, fc_filename=self.fc_filename,
-                                 )
 
         self.model = nn.Sequential(
             self.backbone,
             self.spatial,
-            self.policy
         )
 
     # Defining the forward pass
@@ -43,5 +35,3 @@ class PolicyLearnerBackboneTSM(nn.Module):
         if self.spatial_train:
             self.backbone.save_model(self.backbone_filename)
             self.spatial.save_model(self.spatial_filename)
-        if self.policy_train:
-            self.policy.save_model(lstm_filename=self.lstm_filename, fc_filename=self.fc_filename)
