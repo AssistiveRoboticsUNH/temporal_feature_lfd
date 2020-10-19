0d67268f67b421beba8eccf489bbc5830cd458d3
mbc2004 <mbc2004@cs.unh.edu>
2020-10-19 14:50:52 -0400

    print base_model

--- model/backbone_model/tsm/tsm.py
+++ model/backbone_model/tsm/tsm.py
@@ -73,6 +73,10 @@ class TSMWrapper(TSN):
                 new_k = '.'.join(k.split('.')[1:])
                 new_state_dict[new_k] = v
 
+            print("new_state_dict")
+            for k, v in checkpoint.items():
+                print(k)
+
         self.base_model.load_state_dict(new_state_dict, strict=not training)
 
     def forward(self, inp):
