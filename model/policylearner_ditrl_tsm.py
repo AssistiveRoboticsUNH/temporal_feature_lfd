a4bb29b777dd413f03a1aa5af0b581c298a14985
mbc2004 <mbc2004@cs.unh.edu>
2020-10-19 23:53:53 -0400

    extend DITRL to policy learning

--- model/policylearner_ditrl_tsm.py
+++ model/policylearner_ditrl_tsm.py
@@ -10,9 +10,10 @@ from .policy.policy_lstm import PolicyLSTM
 
 class PolicyLearnerDITRLTSM(nn.Module):
     def __init__(self, lfd_params, filename,
-                 spatial_train=False,
-                 ditrl_pipeline_train=False,
-                 temporal_train=False,
+                 use_feature_extractor=True,
+                 spatial_train=False, use_spatial=True,
+                 ditrl_pipeline_train=False, use_pipeline=True,
+                 temporal_train=False, use_temporal=True,
                  policy_train=False):
         super().__init__()
         self.lfd_params = lfd_params
@@ -21,7 +22,11 @@ class PolicyLearnerDITRLTSM(nn.Module):
         self.spatial_train = spatial_train
         self.ditrl_pipeline_train = ditrl_pipeline_train
         self.temporal_train = temporal_train
-        self.policy_train = policy_train
+
+        self.use_feature_extractor = use_feature_extractor  # use to get features for IAD
+        self.use_spatial = use_spatial  # use to get classification from IAD
+        self.use_pipeline = use_pipeline  # use to get ITRs from IAD
+        self.use_temporal = use_temporal  #use to learn from ITRs
 
         # model filenames
         self.filename = filename
@@ -35,34 +40,53 @@ class PolicyLearnerDITRLTSM(nn.Module):
         self.fc_filename = ".".join([self.filename, "policy", "pt"])
 
         # model sections
-        self.backbone = BackboneTSM(lfd_params, is_training=self.spatial_train, filename=self.backbone_filename)
-        self.bottleneck = SpatialBottleneck(lfd_params, is_training=self.spatial_train, filename=self.bottleneck_filename)
-        self.spatial = SpatialExtLinear(lfd_params, is_training=self.spatial_train, filename=self.spatial_filename)
-        self.pipeline = TemporalPipeline(lfd_params, is_training=self.ditrl_pipeline_train, filename=self.pipeline_filename)
-        self.temporal = TemporalExtLinear(lfd_params, is_training=self.temporal_train, filename=self.temporal_filename)
-        self.policy = PolicyLSTM(lfd_params, is_training=self.policy_train, lstm_filename=self.lstm_filename, fc_filename=self.fc_filename)
-
-        self.model = nn.Sequential(
-            self.backbone,
-            self.bottleneck,
-            self.spatial,
-            self.pipeline,
-            self.temporal,
-            self.policy
-        )
+        if use_feature_extractor:
+            self.backbone = BackboneTSM(lfd_params, is_training=self.spatial_train, trim_model=True,
+                                        filename=lfd_params.args.pretrain_modelname if spatial_train else self.backbone_filename)
+            self.bottleneck = SpatialBottleneck(lfd_params, is_training=self.spatial_train,
+                                                filename=self.bottleneck_filename,
+                                                bottleneck_size=lfd_params.args.bottleneck_size)
+        if use_spatial:
+            self.spatial = SpatialExtLinear(lfd_params, is_training=self.spatial_train,
+                                            filename=self.spatial_filename,
+                                            input_size=lfd_params.args.bottleneck_size,
+                                            consensus="max")
+        if use_pipeline:
+            self.pipeline = TemporalPipeline(lfd_params, is_training=self.ditrl_pipeline_train,
+                                             filename=self.pipeline_filename)
+        if use_temporal:
+            self.temporal = TemporalExtLinear(lfd_params, is_training=self.temporal_train,
+                                              filename=self.temporal_filename,
+                                              input_size=(lfd_params.args.bottleneck_size**2 * 7),
+                                              output_size=8)
+            self.policy = PolicyLSTM(lfd_params, is_training=policy_train,
+                                     lstm_filename=self.lstm_filename, fc_filename=self.fc_filename)
 
     # Defining the forward pass
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, x, act_x):
+        # print("classifier_ditrl_tsm.py: x.shape 0:", x.shape)
+
+        if self.use_feature_extractor:
+            x = self.backbone(x)
+            x = self.bottleneck(x)
+        if self.use_spatial:
+            x = self.spatial(x)
+        if self.use_pipeline:
+            x = self.pipeline(x)
+        if self.use_temporal:
+            x = self.temporal(x)
+            x = self.policy(x, act_x)
+
+        return x
 
     def save_model(self):
-        if self.spatial_train:
+        if self.use_feature_extractor and self.spatial_train:
             self.backbone.save_model(self.backbone_filename)
             self.bottleneck.save_model(self.bottleneck_filename)
+        if self.use_spatial and self.spatial_train:
             self.spatial.save_model(self.spatial_filename)
-        if self.ditrl_pipeline_train:
+        if self.use_pipeline and self.ditrl_pipeline_train:
             self.pipeline.save_model(self.pipeline_filename)
-        if self.temporal_train:
+        if self.use_temporal and self.temporal_train:
             self.temporal.save_model(self.temporal_filename)
-        if self.policy_train:
             self.policy.save_model(lstm_filename=self.lstm_filename, fc_filename=self.fc_filename)
