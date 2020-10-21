40ea8bd8d18d4b973d59f23eadf267934d623ccb
mbc2004 <mbc2004@cs.unh.edu>
2020-10-20 23:00:59 -0400

    updated I3D pretrain model location

--- model/backbone_model/backbone_i3d.py
+++ model/backbone_model/backbone_i3d.py
@@ -45,7 +45,7 @@ class BackboneI3D(InceptionI3d):
             x = torch.transpose(x, 2, 1)
             print("x.size()[2:]", x.size()[2:], type(x.size()[2:]))
 
-            feat_len = torch.prod(x.size()[2:])
+            feat_len = x.size()[3] * x.size()[4] * x.size()[5]
             x = x.view((-1, feat_len))
             print("backbone x.shape4.5:", x.shape)
             return x
