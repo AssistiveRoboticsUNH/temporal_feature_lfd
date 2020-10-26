6c3ce267a9af847759ce2ef0532070f89b293e38
mbc2004 <mbc2004@cs.unh.edu>
2020-10-26 11:10:14 -0400

    debug ablation tests

--- analysis/evaluate_trace_graph_output.py
+++ analysis/evaluate_trace_graph_output.py
@@ -25,15 +25,40 @@ def view_accuracy(df):
     df["obs_label"] = df["filename"].str.split('_').str[0]
     print(df["obs_label"])
 
+    obs = []
+    time = []
+    action = []
+    for i in range(len(df)):
+        row = df.iloc[i]
+        print("row:", row, row["obs_label"])
+        obs.append(row["obs_label"])
+        time.extend([0, 1, 2])
+
+        action.append(row["predicted_label_0"])
+        action.append(row["predicted_label_1"])
+        action.append(row["predicted_label_2"])
+
+    print("len(obs), len(time), len(action):", len(obs), len(time), len(action))
+
+    new_df = pd.DataFrame({"obs": obs, "time": time, "action": action})
+
+    """
     '''
     new_df = pd.DataFrame({"obs": df["obs_label"],
                            "predicted_label_0": df["predicted_label_0"],
                            "predicted_label_1": df["predicted_label_1"],
                            "predicted_label_2": df["predicted_label_2"]})
     '''
-    new_df = df[df["obs"] == 'rrr']["expected_label_0"]
+    new_df = pd.DataFrame({"obs": df["obs_label"],
+                           "expected_label_0": df["expected_label_0"],
+                           "expected_label_1": df["expected_label_1"],
+                           "expected_label_2": df["expected_label_2"]}, dtype="category")
+    """
+    #new_df = new_df[new_df["obs"] == 'rrr']["expected_label_0"]
     print(new_df)
-    print(new_df.groupby("obs").mean())
+    #print(new_df.groupby("obs").mean())
+    new_df.plot.bar()
+    plt.show()
 
 
 
