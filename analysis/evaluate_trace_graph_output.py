11cd1fa343d89e62cc9a1cb4104e4ad735f5f31a
mbc2004 <mbc2004@cs.unh.edu>
2020-10-26 09:11:51 -0400

    corrected label output for ablation

--- analysis/evaluate_trace_graph_output.py
+++ analysis/evaluate_trace_graph_output.py
@@ -17,11 +17,11 @@ def get_accuracy(df):
 
 
 def view_accuracy(df):
-    df["obs_name"]
     print(df)
 
 
 if __name__ == '__main__':
+    '''
     parser = argparse.ArgumentParser(description='Generate IADs from input files')
     parser.add_argument('input_file', help='the checkpoint file to use with the model')
     parser.add_argument('--fig_dir', default="analysis/fig", help='the checkpoint file to use with the model')
@@ -32,12 +32,15 @@ if __name__ == '__main__':
     fig_dir = os.path.join(args.fig_dir, src_filename)
     if not os.path.exists(fig_dir):
         os.makedirs(fig_dir)
+    '''
+    input_file = "csv_output/output_policy_learning_ditrl_i3d_action_trace_ablation.csv"
 
-    df = pd.read_csv(args.input_file)
+    #df = pd.read_csv(args.input_file)
+    df = pd.read_csv(input_file)
 
     acc = get_accuracy(df)
     view_accuracy(df)
     # viz_confusion_matrix(df, os.path.join(fig_dir, "cm.png"))
 
-    print("filename: ", src_filename)
+    #print("filename: ", src_filename)
     print("accuracy: ", acc)
