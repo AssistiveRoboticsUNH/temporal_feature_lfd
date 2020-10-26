20bb8155647a44b1369febf2ed70d3e7ad322025
mbc2004 <mbc2004@cs.unh.edu>
2020-10-26 16:33:15 -0400

    add log_dir to exec files

--- exec_policy_learning_backbone_tsm.py
+++ exec_policy_learning_backbone_tsm.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
         os.makedirs(dir_name)
     filename = os.path.join(dir_name, "model")
 
-    lfd_params = default_model_args(save_id=save_id, log_dir=dir_name, verbose=True)  # parse_model_args()
+    lfd_params = default_model_args(save_id=save_id, log_dir=dir_name)  # parse_model_args()
 
     if TRAIN:
         model = PolicyLearnerBackboneTSM(lfd_params, filename, spatial_train=True, policy_train=True)
 
-        model = train(lfd_params, model)
+        model = train(lfd_params, model, verbose=True)
         model.save_model()
 
     if EVAL:
