9f572253880b2689fb16c83f64afa63705359aa4
mbc2004 <mbc2004@cs.unh.edu>
2020-10-25 21:42:12 -0400

    updated bottleneck to be consistent across I3D and TSM

--- model/spatial/spatial_bottleneck.py
+++ model/spatial/spatial_bottleneck.py
@@ -34,12 +34,13 @@ class SpatialBottleneck(nn.Module):
 	def forward(self, x):
 		print("bottleneck 0", x.shape)
 		#x = x.view((-1, self.lfd_params.args.num_segments) + x.size()[1:])
-		x = x.view(-1, self.input_size, 7, 7)
+		#x = x.view(-1, self.input_size, 7, 7) # TSM
+		x = x.view(-1, self.input_size, 7, 7)  # I3D
 		#x = x.view(-1, self.lfd_params.args.num_segments, self.input_size, 7, 7)
 		print("bottleneck 1", x.shape)
 		x = self.bottleneck(x)
 		print("bottleneck 2", x.shape)
-		x = x.view(-1, self.lfd_params.args.num_segments, self.bottleneck_size)
+		x = x.view(self.lfd_params.args.batch_size, -1, self.bottleneck_size)
 		print("bottleneck 3", x.shape)
 		return x
 
