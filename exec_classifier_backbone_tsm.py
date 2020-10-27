16d3c27554c63423077a06fd2ee835b620085238
mbc2004 <mbc2004@cs.unh.edu>
2020-10-26 22:48:38 -0400

    check classifier

--- exec_classifier_backbone_tsm.py
+++ exec_classifier_backbone_tsm.py
@@ -3,22 +3,30 @@ from parameter_parser import parse_model_args, default_model_args
 from run_classification import train, evaluate
 from model.classifier_backbone_tsm import ClassifierBackboneTSM
 
+TRAIN = True
+EVAL = False
+
 if __name__ == '__main__':
 
-    lfd_params = default_model_args(epochs=1)  # parse_model_args()
-
-    dir_name = "saved_models/classifier_backbone_tsm"  # lfd_params
+    save_id = "classifier_backbone_tsm"
+    dir_name = os.path.join("saved_models", save_id)  # lfd_params
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
     filename = os.path.join(dir_name, "model")
 
-    model = ClassifierBackboneTSM(lfd_params, filename, spatial_train=True)
+    lfd_params = default_model_args(save_id=save_id, log_dir=dir_name)  # parse_model_args()
 
-    model = train(lfd_params, model, verbose=True)
-    model.save_model()
+    if TRAIN:
+        model = ClassifierBackboneTSM(lfd_params, filename, spatial_train=True)
 
-    df = evaluate(lfd_params, model)
+        model = train(lfd_params, model, verbose=True)
+        model.save_model()
 
-    out_filename = os.path.join(lfd_params.args.output_dir, "output_" + lfd_params.args.save_id + ".csv")
-    df.to_csv(out_filename)
-    print("Output placed in: " + out_filename)
+    if EVAL:
+        model = ClassifierBackboneTSM(lfd_params, filename, spatial_train=False)
+
+        df = evaluate(lfd_params, model)
+        out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_single_action.csv")
+        df.to_csv(out_filename)
+        print("Output placed in: " + out_filename)
+
