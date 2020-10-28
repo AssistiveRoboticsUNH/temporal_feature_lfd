bd12edb0e5784a0f8511a2d05bcbeea06fd313bd
mbc2004 <mbc2004@cs.unh.edu>
2020-10-28 09:53:49 -0400

    debug backbone

--- exec_classifier_backbone_tsm.py
+++ exec_classifier_backbone_tsm.py
@@ -25,7 +25,12 @@ if __name__ == '__main__':
     if EVAL:
         model = ClassifierBackboneTSM(lfd_params, filename, spatial_train=False)
 
-        df = evaluate(lfd_params, model)
+        train_df = evaluate(lfd_params, model, mode="train")
+        train_df["mode"] = ["train"]*len(train_df)
+        eval_df = evaluate(lfd_params, model, mode="evaluation")
+        train_df["mode"] = ["evaluation"] * len(eval_df)
+        df = train_df + eval_df
+
         out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_single_action.csv")
         df.to_csv(out_filename)
         print("Output placed in: " + out_filename)
