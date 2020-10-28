21cc4b67e816b9dfe63355535430f57f77663c98
mbc2004 <mbc2004@cs.unh.edu>
2020-10-28 09:27:13 -0400

    new trace file

--- run_policy_learning.py
+++ run_policy_learning.py
@@ -58,8 +58,8 @@ def train(lfd_params, model, verbose=False, input_dtype="video"):
 
 
                 # constrain size to a history of 5 timesteps
-                obs = obs[:, -1:]
-                act = act[:, -1:]
+                obs = obs[:, -5:]
+                act = act[:, -5:]
 
                 # obtain label
                 label = act[:, -1]
