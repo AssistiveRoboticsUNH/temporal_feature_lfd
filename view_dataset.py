424f31b992f4ceaf5c11177816406d23da907311
mbc2004 <mbc2004@cs.unh.edu>
2020-10-29 00:35:12 -0400

    view dataset

--- view_dataset.py
+++ view_dataset.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
     img_dict = {}
 
-    root_path = os.path.join("/home/mbc2004/", "datasets/BlockConstruction/frames/", "train")
+    root_path = os.path.join("/home/mbc2004/", "datasets/BlockConstruction/")
     print("root_path:", root_path)
 
     for mode in ["train", "evaluation"]:
