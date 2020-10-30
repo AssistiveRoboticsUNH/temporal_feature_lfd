fc92d4e286dc10f46deb6021148feb74d4afcb07
mbc2004 <mbc2004@cs.unh.edu>
2020-10-30 02:08:46 -0400

    increase number of frames

--- exec_policy_learning_ditrl_tsm.py
+++ exec_policy_learning_ditrl_tsm.py
@@ -69,7 +69,7 @@ def main(save_id, train_p, eval_p):
         model = PolicyLearnerDITRLTSM(lfd_params, filename, use_feature_extractor=False, use_spatial=False,
                                       use_pipeline=False, use_temporal=True,
                                       spatial_train=False, ditrl_pipeline_train=False, temporal_train=False)
-
+        '''
         df = evaluate_single_action(lfd_params, model, input_dtype="itr")
 
         out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_single_action.csv")
@@ -81,10 +81,14 @@ def main(save_id, train_p, eval_p):
         out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_action_trace.csv")
         df.to_csv(out_filename)
         print("Output placed in: " + out_filename)
+        '''
+        df = evaluate_action_trace(lfd_params, model, input_dtype="itr", ablation=True, verbose=True, mode="train")
+        out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_action_trace_ablation_train.csv")
+        df.to_csv(out_filename)
+        print("Output placed in: " + out_filename)
 
-        df = evaluate_action_trace(lfd_params, model, input_dtype="itr", ablation=True)
-
-        out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_action_trace_ablation.csv")
+        df = evaluate_action_trace(lfd_params, model, input_dtype="itr", ablation=True, verbose=True, mode="evaluation")
+        out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_action_trace_ablation_eval.csv")
         df.to_csv(out_filename)
         print("Output placed in: " + out_filename)
 
