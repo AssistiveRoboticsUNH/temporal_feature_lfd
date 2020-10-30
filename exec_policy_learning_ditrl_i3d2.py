bffdd224d18b3993bd9099bc6d7fb9ddb58d2ad6
mbc2004 <mbc2004@cs.unh.edu>
2020-10-30 13:59:28 -0400

    I3D and GCN

--- exec_policy_learning_ditrl_i3d2.py
+++ exec_policy_learning_ditrl_i3d2.py
@@ -21,7 +21,7 @@ def main(save_id, train_p, eval_p):
     lfd_params = default_model_args(num_segments=64, save_id=save_id, log_dir=dir_name)  # parse_model_args()
 
     if train_p:
-
+        '''
         print("Training Spatial Features")
         model = ClassifierDITRLI3D(lfd_params, filename, use_feature_extractor=True, use_spatial=True, use_pipeline=False, use_temporal=False,
                                    spatial_train=True)  # ditrl is true but unused
@@ -47,9 +47,9 @@ def main(save_id, train_p, eval_p):
                                    spatial_train=False, ditrl_pipeline_train=False, temporal_train=True)
         model = train(lfd_params, model, input_dtype="itr", verbose=True)  # make sure to use ITRs
         model.save_model()
-        '''
-    if eval_p:
 
+    if eval_p:
+        '''
         
         print("Evaluating Model")
         model = ClassifierDITRLI3D(lfd_params, filename, use_feature_extractor=True, use_spatial=True,
@@ -72,7 +72,6 @@ def main(save_id, train_p, eval_p):
                                       spatial_train=False, ditrl_pipeline_train=False, temporal_train=False)
 
         '''
-        '''
         df = evaluate_single_action(lfd_params, model, input_dtype="itr")
 
         out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_single_action.csv")
@@ -85,7 +84,7 @@ def main(save_id, train_p, eval_p):
         df.to_csv(out_filename)
         print("Output placed in: " + out_filename)
         '''
-        '''
+
         df = evaluate_action_trace(lfd_params, model, input_dtype="itr", ablation=True, verbose=True, mode="train")
         out_filename = os.path.join(lfd_params.args.output_dir,
                                     "output_" + save_id + "_action_trace_ablation_train.csv")
@@ -96,7 +95,7 @@ def main(save_id, train_p, eval_p):
         out_filename = os.path.join(lfd_params.args.output_dir, "output_" + save_id + "_action_trace_ablation_eval.csv")
         df.to_csv(out_filename)
         print("Output placed in: " + out_filename)
-        '''
+
 
 if __name__ == '__main__':
     save_id = "policy_learning_ditrl_i3d_bn16_0"
