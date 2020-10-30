a682f9fa347d0ba8737356369cb4016475458c63
mbc2004 <mbc2004@cs.unh.edu>
2020-10-30 16:16:13 -0400

    I3D and GCN

--- exec_all.py
+++ exec_all.py
@@ -7,8 +7,8 @@ from exec_policy_learning_ditrl_i3d import main as main_ditrl_i3d
 
 if __name__ == '__main__':
     out_df = None
-    for i in range(5):
-        #main_bb_tsm("policy_learning_backbone_tsm_" + str(i), True, True)
+    for i in range(10):
+        #main_bb_tsm(policy_learning_backbone_tsm_" + str(i), True, True)
         #main_bb_i3d("policy_learning_backbone_i3d_" + str(i), True, True)
         #main_ditrl_tsm("policy_learning_ditrl_tsm2_" + str(i), True, True)
         #main_ditrl_i3d("policy_learning_ditrl_i3d_" + str(i), True, True)
