cf51c217290accf4b71fad250865bf7f552cf857
mbc2004 <mbc2004@cs.unh.edu>
2020-10-31 13:39:40 -0400

    GCN

--- run_ditrl_pipeline.py
+++ run_ditrl_pipeline.py
@@ -138,7 +138,7 @@ def generate_itr_files_gcn(lfd_params, model, dataset_mode, verbose=False, backb
         #print("model.pipeline.is_training 2:", model.pipeline.is_training)
         data = net(obs)
         #print("model.pipeline.is_training 3:", model.pipeline.is_training)
-        data = data.detach().cpu().numpy()
+        data = data.detach().cpu()
 
         for n, file in enumerate(filename):
 
