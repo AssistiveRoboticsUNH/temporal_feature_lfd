96fc89955a8783e4712172147e0340e0f735d4ee
mbc2004 <mbc2004@cs.unh.edu>
2020-10-31 14:45:46 -0400

    GCN

--- datasets/dataset_gcn_trace.py
+++ datasets/dataset_gcn_trace.py
@@ -76,9 +76,8 @@ class DatasetGCNTrace(DatasetGCN):
     def parse_obs(self, filename_list):
         file_data = []
         for filename in filename_list:
-            obs_data = np.zeros(self.data_shape)
-            if filename != "None":
-                obs_data = super().parse_obs(filename)
+
+            obs_data = super().parse_obs(filename)
             file_data.append(obs_data)
         return np.stack(file_data).squeeze(axis=1)
 
