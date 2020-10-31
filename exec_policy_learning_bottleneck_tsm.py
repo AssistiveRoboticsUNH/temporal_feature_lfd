451f65ed0543f9f4f556c20a8cba4f6c30f247aa
mbc2004 <mbc2004@cs.unh.edu>
2020-10-31 16:47:13 -0400

    GCN

--- exec_policy_learning_bottleneck_tsm.py
+++ exec_policy_learning_bottleneck_tsm.py
@@ -8,7 +8,7 @@ from run_policy_learning import train, evaluate_single_action, evaluate_action_t
 from model.classifier_ditrl_tsm import ClassifierDITRLTSM
 from model.policylearner_bottleneck_tsm import PolicyLearnerBottleneckTSM
 
-TRAIN = False#True
+TRAIN = True#True
 EVAL = True
 
 def main(save_id, train_p, eval_p):
