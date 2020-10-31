9292e936cdf5f5195e1aae8472f29ad43173bfa0
mbc2004 <mbc2004@cs.unh.edu>
2020-10-31 08:19:18 -0400

    I3D and GCN

--- model/temporal/temporal_ext_gcn.py
+++ model/temporal/temporal_ext_gcn.py
@@ -36,7 +36,7 @@ class TemporalExtGCN(nn.Module):
 
     # Defining the forward pass
     def forward(self, x):
-        x = torch.view(x, (-1, self.node_size, self.node_size, self.num_relations))
+        x = torch.reshape(x, (-1, self.node_size, self.node_size, self.num_relations))
 
         edge_idx = []  # [2, num_edges] edge connections (COO format)
         edge_attr = [] # [1, num_edges] type of relationship (ITR)
