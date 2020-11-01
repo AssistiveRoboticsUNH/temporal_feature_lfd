0bcb7e883007a9843dc46d4e3065c37e06556383
mbc2004 <mbc2004@cs.unh.edu>
2020-10-31 22:50:44 -0400

    GCN

--- model/backbone_model/backbone_r21d.py
+++ model/backbone_model/backbone_r21d.py
@@ -130,7 +130,7 @@ class BackboneR21D(nn.Module):
 
         #print("x out:")
         #print(x)
-        x = x.view((-1, self.lfd_params.args.num_segments) + x.size()[1:])
+        x = x.view((-1, 8) + x.size()[1:])
         #x = x.view((-1, self.lfd_params.args.num_segments) + x.size()[1:])
 
         return x
