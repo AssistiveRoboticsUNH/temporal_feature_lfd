07b11595b6372ffe78e8c3d25e36e50b7c41f123
mbc2004 <mbc2004@cs.unh.edu>
2020-11-10 15:11:33 -0500

    add WRN exec_all

--- model_def.py
+++ model_def.py
@@ -28,7 +28,7 @@ def define_model(model_p):
         dense_sample = False
         dense_rate = 0
     elif model_p == "vgg":
-        num_segments = 16
+        num_segments = 32
         bottleneck_size = 16
         dense_sample = False
 
