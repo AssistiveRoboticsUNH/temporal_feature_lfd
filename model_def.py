0e22d310e61b8be4ca600bba588ee476e916cb75
mbc2004 <mbc2004@cs.unh.edu>
2020-11-10 14:39:25 -0500

    add WRN exec_all

--- model_def.py
+++ model_def.py
@@ -29,7 +29,7 @@ def define_model(model_p):
         dense_rate = 0
     elif model_p == "vgg":
         num_segments = 16
-        bottleneck_size = 32
+        bottleneck_size = 16
         dense_sample = False
         dense_rate = 0
     elif model_p == "wrn":
