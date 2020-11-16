5d44a995c19935fbf3e354a47d45299f06c2f16a
mbc2004 <mbc2004@cs.unh.edu>
2020-11-16 17:41:00 -0500

    Generate paper results

--- exec_policy_learning_backbone.py
+++ exec_policy_learning_backbone.py
@@ -61,6 +61,6 @@ if __name__ == '__main__':
     MODEL = sys.argv[1]
 
     #save_id = "policy_learning_ditrl_"+MODEL  # "policy_learning_ditrl_tsm_bn16_2"
-    #save_id = "classifier_bottleneck_wrn1"
-    save_id = "classifier_bottleneck_vgg0"
+    save_id = "classifier_bottleneck_wrn1"
+    #save_id = "classifier_bottleneck_vgg0"
     main(save_id, TRAIN, EVAL, MODEL)
