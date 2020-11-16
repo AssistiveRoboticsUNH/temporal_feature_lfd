1362aeec948ac4d84e7d38386fb99fe58fbdea0a
mbc2004 <mbc2004@cs.unh.edu>
2020-11-16 16:02:32 -0500

    Generate paper results

--- model/classifier.py
+++ model/classifier.py
@@ -6,33 +6,42 @@ from .spatial.spatial_ext_linear import SpatialExtLinear
 
 class Classifier(nn.Module):
     def __init__(self, lfd_params, filename, backbone_id,
-                 spatial_train=False):
+                 feature_extractor_train=False, use_feature_extractor=False, spatial_train=False, use_spatial=True):
         super().__init__()
 
         self.lfd_params = lfd_params
         self.backbone_id = backbone_id
 
         # parts of model to train
+        self.feature_extractor_train = feature_extractor_train
         self.spatial_train = spatial_train
+
+        self.use_feature_extractor = use_feature_extractor  # use to get features for IAD
+        self.use_spatial = use_spatial  # use to get classification from IAD
 
         # model filenames
         self.filename = filename
         self.spatial_filename = ".".join([self.filename, "spatial", "pt"])
 
         # model sections
-        self.feature_extractor = FeatureExtractor(lfd_params, filename, backbone_id, backbone_train=spatial_train)
-        self.spatial = SpatialExtLinear(lfd_params, is_training=self.spatial_train,
-                                        filename=self.spatial_filename,
-                                        input_size=self.feature_extractor.num_output_features,
-                                        consensus="max")
+        if self.use_feature_extractor:
+            self.feature_extractor = FeatureExtractor(lfd_params, filename, backbone_id, backbone_train=spatial_train)
+        if self.use_spatial:
+            self.spatial = SpatialExtLinear(lfd_params, is_training=self.spatial_train,
+                                            filename=self.spatial_filename,
+                                            input_size=self.feature_extractor.num_output_features,
+                                            consensus="max")
 
     # Defining the forward pass
     def forward(self, x):
-        x = self.feature_extractor(x)
-        x = self.spatial(x)
+        if self.use_feature_extractor:
+            x = self.feature_extractor(x)
+        if self.use_spatial:
+            x = self.spatial(x)
         return x
 
     def save_model(self):
-        if self.spatial_train:
+        if self.use_feature_extractor and self.feature_extractor_train:
             self.feature_extractor.save_model()
+        if self.use_spatial and self.spatial_train:
             self.spatial.save_model(self.spatial_filename)
--- model/policy_learner.py
+++ model/policy_learner.py
@@ -6,9 +6,11 @@ from .policy.policy_lstm import PolicyLSTM
 
 class PolicyLearner(Classifier):
     def __init__(self, lfd_params, filename, backbone_id,
-                 backbone_train=False, bottleneck_train=False, use_bottleneck=False, policy_train=False):
+                 use_feature_extractor=False, spatial_train=False, use_spatial=True,
+                 policy_train=False):
         super().__init__(lfd_params, filename, backbone_id,
-                         backbone_train=backbone_train, bottleneck_train=bottleneck_train, use_bottleneck=use_bottleneck)
+                         use_feature_extractor=use_feature_extractor,
+                         spatial_train=spatial_train, use_spatial=use_spatial)
 
         # parts of model to train
         self.policy_train = policy_train
