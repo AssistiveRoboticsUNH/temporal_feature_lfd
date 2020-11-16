8933df242adfbfe80637c199ad4269eb287118dd
mbc2004 <mbc2004@cs.unh.edu>
2020-11-16 15:55:31 -0500

    Generate paper results

--- exec_policy_learning_backbone.py
+++ exec_policy_learning_backbone.py
@@ -57,9 +57,8 @@ if __name__ == '__main__':
 
     import sys
     MODEL = sys.argv[1]
-    FULL = int(sys.argv[2])
 
     #save_id = "policy_learning_ditrl_"+MODEL  # "policy_learning_ditrl_tsm_bn16_2"
     #save_id = "classifier_bottleneck_wrn1"
     save_id = "classifier_bottleneck_vgg0"
-    main(save_id, TRAIN, EVAL, MODEL, full_p=FULL)
+    main(save_id, TRAIN, EVAL, MODEL)
