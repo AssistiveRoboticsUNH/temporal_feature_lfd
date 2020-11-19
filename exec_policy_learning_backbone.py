095c6ed88c97fec3b5362eb5e4244ee48f191805
mbc2004 <mbc2004@cs.unh.edu>
2020-11-19 12:38:16 -0500

    add better evaluation storage

--- exec_policy_learning_backbone.py
+++ exec_policy_learning_backbone.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
         save_id = "classifier_bottleneck_i3d0"
 
     new_save_id = "policy_learning_backbone_" + model_p
-    old_save_dir = os.path.join("saved_models", save_id)
+    old_save_dir = os.path.join("base_models", save_id)
     new_save_dir = os.path.join("saved_models", new_save_id)
     if not os.path.exists(new_save_dir):
         os.makedirs(new_save_dir)
