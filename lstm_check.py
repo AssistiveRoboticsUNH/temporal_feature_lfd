81e2f03a962ce51391313f4d6f23aa009392e3b5
mbc2004 <mbc2004@cs.unh.edu>
2020-11-23 09:12:32 -0500

    add LSTM temporal learning

--- lstm_check.py
+++ lstm_check.py
@@ -11,7 +11,7 @@ np.random.seed(0)
 
 
 def gen_data_duration_inv(length, label):
-    idx = np.random.choice(list(range(length)), 3, replace=False)
+    idx = np.random.choice(list(range(length)), 2, replace=False)
 
     toggle = 0
     iad = np.zeros((length, 3))
