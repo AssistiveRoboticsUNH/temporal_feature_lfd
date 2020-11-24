f7182dcbd465e8c7d2578b5b095b121b3f29816c
mbc2004 <mbc2004@cs.unh.edu>
2020-11-24 15:25:07 -0500

    add LSTM temporal learning

--- analysis/get_pl_accuracy.py
+++ analysis/get_pl_accuracy.py
@@ -142,9 +142,10 @@ if __name__ == '__main__':
     elif model_p == "i3d":
         save_id = "output_policy_learning_"+model_type+"_i3d"
 
-    dataset_eval = "csv_output/"+save_id+"_action_trace.csv"
-    abl_train = "csv_output/" + save_id + "_action_trace_ablation_train.csv"
-    abl_eval = "csv_output/" + save_id + "_action_trace_ablation_eval.csv"
+    results_dir = os.path.join("stored_results/linear/", model_p)
+    dataset_eval = os.path.join(results_dir, save_id+"_action_trace.csv")
+    abl_train = os.path.join(results_dir, save_id + "_action_trace_ablation_train.csv")
+    abl_eval = os.path.join(results_dir, save_id + "_action_trace_ablation_eval.csv")
 
     df = pd.read_csv(dataset_eval)
     print("dataset, per_act, eval::", get_accuracy_per_action(df, timesteps=5))
