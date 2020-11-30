0dbfadf46727243d6c771d3fbc8244668d8f6e61
mbc2004 <mbc2004@cs.unh.edu>
2020-11-30 13:46:10 -0500

    feed ITR to LSTM

--- analysis/get_pl_accuracy.py
+++ analysis/get_pl_accuracy.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
     elif model_p == "i3d":
         save_id = "output_policy_learning_"+model_type+"_i3d"
 
-    #results_dir = os.path.join("stored_results/lstm/", model_p)
+    #results_dir = os.path.join("stored_results/linear+lstm/", model_p)
     results_dir = os.path.join("csv_output/")
     dataset_eval = os.path.join(results_dir, save_id+"_action_trace.csv")
     abl_train = os.path.join(results_dir, save_id + "_action_trace_ablation_train.csv")
