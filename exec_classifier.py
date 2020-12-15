769693a05b2b27ddfa39f1cec64475e585a11da9
mbc2004 <mbc2004@cs.unh.edu>
2020-12-15 07:35:27 -0500

    apply Threshold modifications

--- exec_classifier.py
+++ exec_classifier.py
@@ -42,17 +42,17 @@ if __name__ == '__main__':
     '''
     new_save_id = make_model_name(model_p, save_id, "backbone")
     main_bb(new_save_id, gen_p=True, train_p=True, eval_p=True, backbone_id=model_p, use_bottleneck=False)   # backbone
-    '''
+    
     new_save_id = make_model_name(model_p, save_id, "iad")
     main_bb(new_save_id, gen_p=True, train_p=True, eval_p=True, backbone_id=model_p, use_bottleneck=True)   # iad
-    '''
+    
     new_save_id = make_model_name(model_p, save_id, "ditrl")
     main_ditrl(new_save_id, gen_itr=True, gen_vee=True, train_p=True, eval_p=True, backbone_id=model_p)  # ditrl
-
+    '''
     new_save_id = make_model_name(model_p, save_id, "vee")
     main_ditrl(new_save_id, gen_itr=True, gen_vee=True, train_p=False, eval_p=False, backbone_id=model_p)  # make vee only
     main_bb(new_save_id, gen_p=False, train_p=True, eval_p=True, backbone_id=model_p, use_bottleneck=True)  # threshold
-    '''
+
 
     import pandas as pd
     filename = os.path.join("csv_output", "output_"+new_save_id+"_spatial.csv")
