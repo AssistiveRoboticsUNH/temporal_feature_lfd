10a36f53f4d35dbe8b5378ea42844b42bba0daa3
mbc2004 <mbc2004@cs.unh.edu>
2020-12-16 14:27:11 -0500

    complete model

--- model/temporal/temporal_ext_lstm.py
+++ model/temporal/temporal_ext_lstm.py
@@ -93,6 +93,7 @@ class TemporalExtLSTM(nn.Module):
         for i in range(batch_size):
             new_x = np.zeros((self.input_size + 7, max(1, edge_idx.shape[1])), np.float64)
             edge_idxes = np.where(batch == i)
+            print("edge_idxes:", edge_idxes)
 
             for j in edge_idxes:
                 n1 = edge_idx[0, j]
