3ed3f6c6ce293e5186d086760c81be473e870854
mbc2004 <mbc2004@cs.unh.edu>
2020-12-16 14:39:46 -0500

    complete model

--- exec_policy_learning_ditrl_lstm_gcn.py
+++ exec_policy_learning_ditrl_lstm_gcn.py
@@ -7,7 +7,7 @@ from run_policy_learning_gcn import train, evaluate_single_action, evaluate_acti
 from model.classifier_ditrl import ClassifierDITRL
 from model.policy_learner_ditrl import PolicyLearnerDITRL
 
-GENERATE_ITR=False
+GENERATE_ITR=True
 GENERATE_VEE=False
 TRAIN = True
 EVAL = True
