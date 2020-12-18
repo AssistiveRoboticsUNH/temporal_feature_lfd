c972a6e867e460d71902e92d616c1239b76dc862
mbc2004 <mbc2004@cs.unh.edu>
2020-12-18 07:05:22 -0500

    complete model

--- model/temporal/temporal_ext_gcn.py
+++ model/temporal/temporal_ext_gcn.py
@@ -28,13 +28,13 @@ class TemporalExtGCN(nn.Module):
         # CONSIDER STACKED (will need ReLU, check on actual ITR data)
         #self.gcn = GCNConv(self.node_size, self.hidden_size)
 
-        #self.gcn1 = GCNConv(self.node_size, self.hidden_size)
-        #self.gcn2 = GCNConv(self.hidden_size, self.hidden_size)
-        self.gcn3 = GCNConv(self.hidden_size, self.hidden_size)
+        self.gcn1 = GCNConv(self.node_size, self.hidden_size)
+        self.gcn2 = GCNConv(self.hidden_size, self.hidden_size)
+        #self.gcn3 = GCNConv(self.hidden_size, self.hidden_size)
         #self.gcn4 = GCNConv(self.hidden_size, self.hidden_size)
 
-        self.gcn1 = RGCNConv(self.node_size, self.hidden_size, num_relations=self.num_relations)
-        self.gcn2 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)
+        #self.gcn1 = RGCNConv(self.node_size, self.hidden_size, num_relations=self.num_relations)
+        #self.gcn2 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)
         #self.gcn3 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)
         #self.gcn4 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)
         #self.densegcn = gnn.DenseGCNConv(self.hidden_size, self.output_size)
@@ -66,8 +66,11 @@ class TemporalExtGCN(nn.Module):
         print("temp_ext_gcn edge_idx:", edge_idx.shape, type(edge_idx), edge_idx.dtype)
         print("temp_ext_gcn edge_attr:", edge_attr.shape, type(edge_attr), edge_attr.dtype)
 
-        x = F.relu(self.gcn1(x, edge_idx, edge_attr))
-        x = F.relu(self.gcn2(x, edge_idx, edge_attr))
+        #x = F.relu(self.gcn1(x, edge_idx, edge_attr))
+        #x = F.relu(self.gcn2(x, edge_idx, edge_attr))
+        x = F.relu(self.gcn1(x, edge_idx))
+        x = F.relu(self.gcn2(x, edge_idx))
+
         #x = F.relu(self.gcn3(x, edge_idx))
         #x = F.relu(self.gcn4(x, edge_idx, edge_attr))
 
