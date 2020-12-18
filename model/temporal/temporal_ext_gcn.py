7f7721bfb828929de35f16cebba886a68af5c2e1
mbc2004 <mbc2004@cs.unh.edu>
2020-12-18 07:51:36 -0500

    complete model

--- model/temporal/temporal_ext_gcn.py
+++ model/temporal/temporal_ext_gcn.py
@@ -18,7 +18,7 @@ class TemporalExtGCN(nn.Module):
         self.filename = filename
 
         # constants params
-        self.num_relations = 0#num_relations
+        self.num_relations = 7#num_relations
         self.node_size = node_size
         self.hidden_size = 512
         self.output_size = output_size
@@ -60,7 +60,7 @@ class TemporalExtGCN(nn.Module):
         edge_attr = edge_attr.cuda()
         batch = batch.cuda()
 
-        #edge_attr[:] = 0
+        edge_attr[:] = 0
 
         print("temp_ext_gcn node_x:", x.shape, type(x), x.dtype)
         print("temp_ext_gcn edge_idx:", edge_idx.shape, type(edge_idx), edge_idx.dtype)
