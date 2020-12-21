b682768356e721fed1201007649feec9375da741
mbc2004 <mbc2004@cs.unh.edu>
2020-12-20 22:51:52 -0500

    investigate classifier policy learning difference

--- run_policy_learning_gcn.py
+++ run_policy_learning_gcn.py
@@ -70,8 +70,8 @@ def train(lfd_params, model, verbose=False, input_dtype="video"):
                 #print(obs[0].x)
 
                 # constrain size to a history of 5 timesteps
-                obs = obs[-5:]
-                act = act[-5:]
+                obs = obs[-3:]
+                act = act[-3:]
 
                 obs = Batch.from_data_list(obs)
                 print("act.shape:", act.shape, act)
@@ -174,8 +174,8 @@ def evaluate_single_action(lfd_params, model, mode="evaluation", verbose=False, 
                 a = act[:j]
 
                 # constrain size to a history of 5 timesteps
-                o = o[-5:]
-                a = a[-5:]
+                o = o[-3:]
+                a = a[-3:]
 
                 o = Batch.from_data_list(o)
 
