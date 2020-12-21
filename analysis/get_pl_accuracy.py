ccdbca4b800b3252af7798c1f18557107223132b
mbc2004 <mbc2004@cs.unh.edu>
2020-12-20 22:07:50 -0500

    investigate classifier policy learning difference

--- analysis/get_pl_accuracy.py
+++ analysis/get_pl_accuracy.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
-
+import pandas as pd
 import argparse
 import os
 
@@ -11,7 +11,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 def get_accuracy_per_action(df, timesteps):
     expected = np.concatenate([df["expected_label_"+str(i)] for i in range(timesteps)])
     predicted = np.concatenate([df["predicted_label_"+str(i)] for i in range(timesteps)])
-
+    print(pd.DataFrame({"expected": expected, "predicted:": predicted}))
     return accuracy_score(y_true=expected, y_pred=predicted)
 
 
