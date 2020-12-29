045b0de5ff39c9ab00924c894dd4bb8c557ef22b
mbc2004 <mbc2004@cs.unh.edu>
2020-12-29 16:26:15 -0500

    investigate classifier policy learning difference

--- exec_lstm.py
+++ exec_lstm.py
@@ -48,8 +48,9 @@ if __name__ == '__main__':
     new_save_id = make_model_name(model_p, save_id, "ditrl")
     main_ditrl(new_save_id, gen_itr=True, gen_vee=True, train_p=True, eval_p=True, backbone_id=model_p)  # ditrl
     '''
+    '''
     new_save_id = make_model_name(model_p, save_id, "vee")
     main_ditrl(new_save_id, gen_itr=True, gen_vee=True, train_p=False, eval_p=False, backbone_id=model_p)  # make vee only
     main_bb(new_save_id, gen_p=False, train_p=True, eval_p=True, backbone_id=model_p, use_bottleneck=True)  # threshold
-
+    '''
     print("done")
