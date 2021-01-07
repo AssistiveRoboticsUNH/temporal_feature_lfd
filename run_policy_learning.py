37b170d3ece0aaa0e80851570ec3f8281f1793d7
mbc2004 <mbc2004@cs.unh.edu>
2021-01-06 22:59:56 -0500

    check learning of no features

--- run_policy_learning.py
+++ run_policy_learning.py
@@ -77,7 +77,10 @@ def train(lfd_params, model, verbose=False, input_dtype="video"):
                 logits = net(obs.float(), act.float())
 
                 # get loss
-                print("label:", label.shape, label.dtype)
+                print("obs:", obs)
+                print("act:", act)
+                print("label:", label)
+                print("")
                 loss = criterion(logits, label.long().cuda())
                 loss.backward()
 
