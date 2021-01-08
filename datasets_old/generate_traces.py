3b95825bf70ad70ec73b933c9bf3abe676d177ce
mbc2004 <mbc2004@cs.unh.edu>
2021-01-08 14:53:21 -0500

    try activation on policy learning

--- datasets_old/generate_traces.py
+++ datasets_old/generate_traces.py
@@ -112,7 +112,7 @@ def gen_path2(length=10):
 
 '''
 just RGB and N perfectly matched to the actions
-'''12
+'''
 def gen_path3(length=10): # only RGBN
     act = [1,1,1,2,2,2,3,3,3, 0, 0, 0]
     random.shuffle(act)
