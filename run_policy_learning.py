f3ca28f1ecf30a32eec8e5f57f6fe9f875a8ef64
mbc2004 <mbc2004@cs.unh.edu>
2021-01-11 13:12:22 -0500

    reduce history to 3

--- run_policy_learning.py
+++ run_policy_learning.py
@@ -243,6 +243,8 @@ def evaluate_action_trace(lfd_params, model, mode="evaluation", verbose=False, i
                 o = o[:, -WIN_HIST:]
                 a_history = a_history[:, -WIN_HIST:]
 
+                print("o:", o)
+
                 # compute output
                 logits = net(o.float(), a_history.float())
 
