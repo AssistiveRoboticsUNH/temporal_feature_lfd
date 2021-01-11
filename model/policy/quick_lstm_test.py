d33f13d7182da843c8ea3b397f77932cfceb5495
mbc2004 <mbc2004@cs.unh.edu>
2021-01-11 10:17:18 -0500

    try activation on policy learning

--- model/policy/quick_lstm_test.py
+++ model/policy/quick_lstm_test.py
@@ -303,7 +303,7 @@ def evaluate_ablation(model, mode="evaluation"):
 if __name__ == '__main__':
     model = Model()
 
-    train(model)
+    #train(model)
     evaluate_action_trace(model, mode="train")
     evaluate_action_trace(model)
 
