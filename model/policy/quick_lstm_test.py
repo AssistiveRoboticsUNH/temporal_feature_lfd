75f68181653fb8535e5ca16440690c4e06133751
mbc2004 <mbc2004@cs.unh.edu>
2021-01-12 07:15:35 -0500

    reduce history to 3

--- model/policy/quick_lstm_test.py
+++ model/policy/quick_lstm_test.py
@@ -35,7 +35,7 @@ class Model(nn.Module):
 class TraceDataset(Dataset):
     def __init__(self, mode="train"):
         super().__init__()
-        self.data = np.load("/home/mbc2004/datasets/BlockConstruction/traces6.npy")
+        self.data = np.load("/home/mbc2004/datasets/BlockConstruction/traces7.npy")
         self.num_obs = 8
         self.num_act = 4
 
@@ -102,8 +102,8 @@ def train(model):
             for i, data_packet in enumerate(data_loader):
 
                 obs, act = data_packet
-                obs = obs[:, -3:]
-                act = act[:, -3:]
+                #obs = obs[:, -3:]
+                #act = act[:, -3:]
                 #print("obs:", obs.shape, obs.dtype)
                 #print(obs)
                 #print("act:", act.shape, act.dtype)
@@ -165,8 +165,8 @@ def evaluate_action_trace(model, mode="evaluation"):
                 o = obs[:, :j]
                 a = act[:, :j]
 
-                o = o[:, -3:]
-                a = a[:, -3:]
+                #o = o[:, -3:]
+                #a = a[:, -3:]
 
                 # obtain label
                 label = a[:, -1]
