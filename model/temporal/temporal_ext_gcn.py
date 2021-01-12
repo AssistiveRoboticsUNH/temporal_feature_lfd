c97c3c6c79ac48f3b1dc2e3c3814065a69507d59
mbc2004 <mbc2004@cs.unh.edu>
2021-01-12 11:51:21 -0500

    reduce history to 3

--- model/temporal/temporal_ext_gcn.py
+++ model/temporal/temporal_ext_gcn.py
@@ -20,7 +20,7 @@ class TemporalExtGCN(nn.Module):
         # constants params
         self.num_relations = 1#num_relations
         self.node_size = node_size
-        self.hidden_size = 512
+        self.hidden_size = 1024#512
         self.output_size = output_size
 
         # define model vars
