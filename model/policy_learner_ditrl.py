fcd5021984958127f9a7eddc59301d7bf2f6e2e5
mbc2004 <mbc2004@cs.unh.edu>
2021-01-12 09:41:42 -0500

    reduce history to 3

--- model/policy_learner_ditrl.py
+++ model/policy_learner_ditrl.py
@@ -42,7 +42,7 @@ class PolicyLearnerDITRL(ClassifierDITRL):
             new_obs[0, r, idx[r]] = 1
         obs_x = torch.as_tensor(new_obs).cuda()
 
-        #print("obs_x_soft:", obs_x)
+        print("obs_x_soft:", obs_x)
         x = self.policy(obs_x, act_x)
         return x
 
--- run_policy_learning_gcn.py
+++ run_policy_learning_gcn.py
@@ -65,13 +65,13 @@ def train(lfd_params, model, verbose=False, input_dtype="video", ablation=False)
                 obs, act, obs_filename, act_filename = data_packet
                 obs = list(obs)
 
-                #print("obs:")
-                #for f in obs_filename:
-                #    print(f)
+                print("obs:")
+                for f in obs_filename:
+                    print(f)
 
                 # constrain size to a history of 5 timesteps
-                obs = obs[-WIN_HIST:]
-                act = act[:, -WIN_HIST:]
+                #obs = obs[-WIN_HIST:]
+                #act = act[:, -WIN_HIST:]
 
                 obs = Batch.from_data_list(obs)
 
