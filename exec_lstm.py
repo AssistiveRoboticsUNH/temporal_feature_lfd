975d7f22383ef6a7a398ea2174914f9948f5ad68
mbc2004 <mbc2004@cs.unh.edu>
2021-01-14 09:05:21 -0500

    increased frame count to 32

--- exec_lstm.py
+++ exec_lstm.py
@@ -39,14 +39,14 @@ if __name__ == '__main__':
     elif model_p == "trn":
         save_id = "classifier_bottleneck_trn2"
 
-    ''' 
+
     new_save_id = make_model_name(model_p, save_id, "backbone")
     main_bb(new_save_id, gen_p=True, train_p=True, eval_p=True, backbone_id=model_p, use_bottleneck=False)   # backbone
-    '''
 
+    '''
     new_save_id = make_model_name(model_p, save_id, "iad")
     main_bb(new_save_id, gen_p=False, train_p=True, eval_p=True, backbone_id=model_p, use_bottleneck=True)   # iad
-    ''' 
+    
     new_save_id = make_model_name(model_p, save_id, "ditrl")
     main_ditrl(new_save_id, gen_itr=True, gen_vee=True, train_p=True, eval_p=True, backbone_id=model_p)  # ditrl
     '''
