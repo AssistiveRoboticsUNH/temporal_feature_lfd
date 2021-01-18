c0eae12f2aa69ab8c338c03ce562ccc519cb6e2a
mbc2004 <mbc2004@cs.unh.edu>
2021-01-18 09:39:12 -0500

    I3D breakdown

--- model/backbone_model/backbone_i3d.py
+++ model/backbone_model/backbone_i3d.py
@@ -57,8 +57,10 @@ class BackboneI3D(InceptionI3d):
         #x = self.avg_pool(x)
 
         print("backbone x.shape5.1:", x.shape)
-        x = torch.max(x, dim=2)
-        x = torch.max(x, dim=2)
+        x = torch.max(x, dim=3)
+        print("backbone x.shape5.1.1:", x.shape)
+        x = torch.max(x, dim=3)
+        print("backbone x.shape5.1.2:", x.shape)
         x = torch.flatten(x, 2, 4)
         print("backbone x.shape5.2:", x.shape)
 
