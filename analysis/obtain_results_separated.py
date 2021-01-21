4c0a823d2df33f25b3d6ce1c25b8b26944984fa0
mbc2004 <mbc2004@cs.unh.edu>
2021-01-21 06:36:30 -0500

    I3D results gen

--- analysis/obtain_results_separated.py
+++ analysis/obtain_results_separated.py
@@ -64,6 +64,7 @@ def breakdown_full(spatial_df, title="", output_filename=""):
         plt.show()
     else:
         plt.savefig(output_filename)
+        print("placed in :", output_filename)
 
 def breakdown_value_only(spatial_df, title="", output_filename=""):
     spatial_df["model"] = ["spatial"] * len(spatial_df)
@@ -98,5 +99,6 @@ if __name__ == "__main__":
     # run analysis
     breakdown_full(spatial_df_src, title=target_label+" Breakdown", output_filename=output_filename_bd)
     #breakdown_value_only(spatial_df_src)
+
 
 
