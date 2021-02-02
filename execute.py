742f6841cd692b996296f943c6f8fea8fcdfea89
mbc2004 <mbc2004@cs.unh.edu>
2021-02-02 07:44:05 -0500

    corrections

--- execute.py
+++ execute.py
@@ -182,7 +182,7 @@ def execute(args, lfd_params, cur_repeat):
     eval_df["mode"] = ["evaluation"] * len(eval_df)
 
     df = pd.concat([train_df, eval_df])
-    save_path = os.path.join(lfd_params.model_save_dir, model.filename)+".csv"
+    save_path = os.path.join(lfd_params.model_save_dir, model.filename, "results.csv")
     print("save_path:", save_path)
     df.to_csv(save_path)
 
