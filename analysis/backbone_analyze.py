9958de09db0ec318b21fa2c3e51d0cbac3308b8b
mbc2004 <mbc2004@cs.unh.edu>
2021-02-02 10:11:02 -0500

    corrections

--- analysis/backbone_analyze.py
+++ analysis/backbone_analyze.py
@@ -7,18 +7,46 @@ def locate_files(src_dir, model):
 
     files = []
     for r, d, f in os.walk(src_dir):
-        print(r, d, f)
         for filename in f:
-            if f == "results.csv":
+            if filename == "results.csv":
                 file_path = os.path.join(r, filename)
-                run_name = r.split('/')[-1]
-                files.append((run_name, file_path))
+                #run_name = r.split('/')[-1]
+                files.append((r, file_path))
     return files
 
 
 def organize_data(files):
-    for f in files:
-        print(f)
+    df_arr = []
+
+    # organize each file by accuracy
+    for run_name, file_path in files:
+        df = pd.read_csv(file_path)
+        df["run_name"] = [run_name]*len(df)
+        df["correct"] = df["expected_label"] == df["predicted_label"]
+        df["correct"] = df["correct"].astype(int)
+
+        df = df.groupby(["mode", "expected_label"]).mean().reset_index()
+        df = pd.DataFrame({"run_name": [run_name]*len(df["mode"]),
+                           "mode": df["mode"],
+                           "expected_label": df["expected_label"],
+                           "accuracy": df["correct"]})
+
+        df_arr.append(df)
+
+    # collect together and present
+    df = pd.concat(df_arr)
+
+    paired_df = {}
+    for mode in ["train", "evaluation"]:
+        for label, label_n in [('n', 0), ('r', 1), ('rr', 2), ('rrr', 3), ('g', 4), ('gb', 5), ('bg', 6), ('b', 7)]:
+            paired_df["run_name"] = df[(df["mode"] == mode) & (df["expected_label"] == label_n)]["run_name"].to_numpy()
+            paired_df[mode+'_'+label] = df[(df["mode"] == mode) & (df["expected_label"] == label_n)]["accuracy"].to_numpy()
+
+    df2 = pd.DataFrame(paired_df)
+    df2 = df2.drop(columns=["train_rr", "train_rrr", "train_bg", "train_gb",
+                            "evaluation_rr", "evaluation_rrr", "evaluation_bg", "evaluation_gb"])
+    print(df2)
+
 
 if __name__ == '__main__':
 
