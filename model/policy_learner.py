aeae72b5d0e89f3817073cb9bfedcc731285945c
mbc2004 <mbc2004@cs.unh.edu>
2021-02-03 11:24:44 -0500

    corrections

--- model/policy_learner.py
+++ model/policy_learner.py
@@ -24,7 +24,7 @@ class PolicyLearner(Classifier):
         self.train_policy = train_policy
 
         # model filenames
-        self.filename = os.path.join(self.lfd_params.model_save_dir, filename)
+        #self.filename = os.path.join(self.lfd_params.model_save_dir, filename) # taken care of by classifer
         #self.lstm_filename = ".".join([self.filename, "lstm", "pt"])
         #self.fc_filename = ".".join([self.filename, "policy", "pt"])
 
