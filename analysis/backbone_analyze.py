4f5695d292d248c2d22b2fbe9742853779767a33
mbc2004 <mbc2004@cs.unh.edu>
2021-02-08 09:32:20 -0500

    correction

--- analysis/backbone_analyze.py
+++ analysis/backbone_analyze.py
@@ -7,13 +7,13 @@ def locate_files(src_dir, model):
 
     files = []
     for r, d, f in os.walk(src_dir):
-        if model in r:
-            print(model, r)
-            for filename in f:
-                if filename == "results.csv":
-                    file_path = os.path.join(r, filename)
-                    #run_name = r.split('/')[-1]
-                    files.append((r, file_path))
+        #if model in r:
+        print(model, r)
+        for filename in f:
+            if filename == "results.csv":
+                file_path = os.path.join(r, filename)
+                #run_name = r.split('/')[-1]
+                files.append((r, file_path))
     return files
 
 
