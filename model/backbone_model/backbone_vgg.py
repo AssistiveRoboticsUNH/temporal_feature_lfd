397e1aeba5fc8e8298432ce8522ec270cd53c2a0
mbc2004 <mbc2004@cs.unh.edu>
2021-02-10 14:45:16 -0500

    correction

--- model/backbone_model/backbone_vgg.py
+++ model/backbone_model/backbone_vgg.py
@@ -25,7 +25,8 @@ class BackboneVGG(nn.Module):
         self.base_model.classifier = nn.Identity()  # remove dropout
 
         # load model parameters
-        assert self.filename is not None, "ERROR: backbone_vgg.py: filename must be defined. Receieved: "+str(self.filename)
+        print("self.filename:", self.filename)
+        assert self.filename is not None, "ERROR: backbone_vgg.py: filename must be defined."
         if not is_training:
             self.load_model(self.filename, is_training)
 
