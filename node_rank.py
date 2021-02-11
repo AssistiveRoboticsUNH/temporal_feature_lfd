1d089e19f3545811882148e0e035b6c7fc9822ef
mbc2004 <mbc2004@cs.unh.edu>
2021-02-10 20:03:04 -0500

    correction

--- node_rank.py
+++ node_rank.py
@@ -12,33 +12,19 @@ from execute import define_model
 
 
 def prune_graph(graph, feature_to_prune):
-    # set node value to 0, (maybe remove)
-    # remove all edges to/from node
-    # remove all edge_attributes
-    #print("prune here:", graph)
     x = graph.x
     edge_index = graph.edge_index
     edge_attr = graph.edge_attr
 
-    #print("x pre:", x.shape)
     node_feature_label = np.argmax(x, axis=1)
     x = x[np.where(node_feature_label != feature_to_prune)]
-    #print("x post:", x.shape)
 
-    #print("edge_idx pre:", edge_index.shape)
     edges_to_remove = set(np.where(edge_index == feature_to_prune)[1].tolist())
-    #print("edge_feature_label:", edge_feature_label)
     edges_to_keep = set(list(range(edge_index.shape[1]))).difference(edges_to_remove)
     edges_to_keep = list(edges_to_keep)
-    #print("edges_to_remove:", edges_to_remove)
+
     edge_index = edge_index[:, edges_to_keep]
-    #print("edge_idx post:", edge_index.shape)
-
-    #print("edge_attr pre:", edge_attr.shape)
     edge_attr = edge_attr[edges_to_keep]
-    #print("edge_attr post:", edge_attr.shape)
-
-    #print(graph.edge_index)
 
     return Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
 
@@ -61,15 +47,13 @@ def evaluate_c_itr(lfd_params, model, mode="evaluation", verbose=False):
     #--------------
     baseline_logits = {}
 
+    print("Obtain Baseline Logits...")
     for i, data_packet in enumerate(data_loader):
         obs, label, filename = data_packet
         expected_label = label.cpu().detach().numpy()[0]
 
         # compute output
         logits = net(obs)
-        #print("logits:", logits.shape)
-        #print(logits)
-        #print("expected_label:", expected_label)
 
         baseline_logits[filename[0]] = logits.detach().cpu().numpy()[0, expected_label]
 
@@ -78,13 +62,13 @@ def evaluate_c_itr(lfd_params, model, mode="evaluation", verbose=False):
     feature_importance_list = []
 
     for feat in range(lfd_params.model.bottleneck_size):
+        print("Obtain Importance for feature ...", feat)
         feature_importance = 0
         for i, data_packet in enumerate(data_loader):
             obs, label, filename = data_packet
             expected_label = label.cpu().detach().numpy()[0]
 
             # prune obs to remove feature
-            print("obs:", obs)
             prune_graph(obs, feat)
 
             # compute output
