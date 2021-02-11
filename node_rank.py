97b2fc324a5cef71c95fddac373c53e2b74f0ccd
mbc2004 <mbc2004@cs.unh.edu>
2021-02-10 19:24:58 -0500

    correction

--- node_rank.py
+++ node_rank.py
@@ -25,7 +25,12 @@ def prune_graph(graph, feature_to_prune):
     print("x post:", x.shape)
 
     print("edge_idx pre:", edge_index.shape)
-    edge_feature_label = np.where(edge_index == feature_to_prune)
+    edge_feature_label = np.where(edge_index == feature_to_prune)[1]
+    print("edge_feature_label:", edge_feature_label)
+    edges_to_remove = [x for x in range(edge_index.shape[1]) if x not in edge_feature_label]
+    print("edges_to_remove:", edges_to_remove)
+    edge_index = edge_index.T
+
     print(edge_feature_label)
 
 
