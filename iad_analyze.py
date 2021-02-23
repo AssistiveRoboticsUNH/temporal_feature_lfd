d3181b08748908efdd3ee25f05f29d36e4febae4
mbc2004 <mbc2004@cs.unh.edu>
2021-02-23 12:45:27 -0500

    view iads and events

--- iad_analyze.py
+++ iad_analyze.py
@@ -72,9 +72,12 @@ def exec_func(args, lfd_params):
         global_avg_values *= global_cnt_values
         for i, v in enumerate(avg_values):
             global_avg_values[i] += v
+
+        print("cnt1:", cnt_values, global_cnt_values)
         global_cnt_values += cnt_values
+        print("cnt2:", cnt_values, global_cnt_values)
         global_cnt_values /= global_cnt_values
-        print("cnt:", cnt_values, global_cnt_values)
+        print("cnt3:", cnt_values, global_cnt_values)
         print("avg:", global_avg_values[:5])
 
 
