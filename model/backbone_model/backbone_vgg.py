98343c4f76292a0126a27e52f4c76fd9c5e74236
mbc2004 <mbc2004@cs.unh.edu>
2021-02-23 07:29:03 -0500

    vgg

--- model/backbone_model/backbone_vgg.py
+++ model/backbone_model/backbone_vgg.py
@@ -54,7 +54,7 @@ class BackboneVGG(nn.Module):
         if self.max_pool_features:
             x, _ = x.max(dim=3, keepdim=True)
             x, _ = x.max(dim=4, keepdim=True)
-            x = torch.flatten(x, start_dim=3)
+            x = torch.flatten(x, start_dim=2)
 
         print("backbone x.shape5:", x.shape)
 
