6ae9b0a4b8d4c9dcb480dd87f49c0976652c175f
mbc2004 <mbc2004@cs.unh.edu>
2021-03-04 22:06:39 -0500

    add TCN

--- analysis/model_analyze.py
+++ analysis/model_analyze.py
@@ -44,14 +44,34 @@ def get_accuracy_pl(df):
         accuracy = accuracy_score(y_true=expected, y_pred=predicted)
         print("accuracy:", accuracy)
 
-        print(df_mode.columns)
+def get_accuracy_pl_obs(df):
+    timesteps = 3
+
+    for mode in ["train", "evaluation"]:
+        print('----'+mode.upper()+'----')
+        df_mode = df[df["mode"] == mode]
+
+        print(df_mode)
+
+        obs_filenames = np.concatenate([df_mode["obs_filename_" + str(i)] for i in range(timesteps)])
+        print(obs_filenames)
+        expected = np.concatenate([df_mode["expected_label_" + str(i)] for i in range(timesteps)])
+        predicted = np.concatenate([df_mode["predicted_label_" + str(i)] for i in range(timesteps)])
+
+        confusion_matrix = pd.crosstab(expected, predicted, rownames=['Expected'], colnames=['Predicted'])
+        print(confusion_matrix)
+
+        accuracy = accuracy_score(y_true=expected, y_pred=predicted)
+        print("accuracy:", accuracy)
+
+
 
 
 def parse_exec_args():
     import argparse
     parser = argparse.ArgumentParser(description='Execute file')
 
-    parser.add_argument('app', help='classifier(c)/policy_learner(pl)', choices=['c', 'pl'])
+    parser.add_argument('app', help='classifier(c)/policy_learner(pl)', choices=['c', 'pl', 'pl_obs'])
     parser.add_argument('model', help='model_location')
 
     return parser.parse_args()
@@ -63,5 +83,7 @@ if __name__ == '__main__':
 
     if args.app == "c":
         get_accuracy_c(df)
-    else:
+    elif args.app == "pl":
         get_accuracy_pl(df)
+    else:
+        get_accuracy_pl_obs(df)
