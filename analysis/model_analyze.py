e989b3b431048aabc43df3a8db945934bd646f7b
mbc2004 <mbc2004@cs.unh.edu>
2021-03-04 22:51:43 -0500

    feature investigation

--- analysis/model_analyze.py
+++ analysis/model_analyze.py
@@ -25,9 +25,6 @@ def get_accuracy_c(df):
         print("accuracy:", accuracy)
 
 
-
-
-
 def get_accuracy_pl(df):
     timesteps = 12
 
@@ -44,6 +41,7 @@ def get_accuracy_pl(df):
         accuracy = accuracy_score(y_true=expected, y_pred=predicted)
         print("accuracy:", accuracy)
 
+
 def get_accuracy_pl_obs(df):
     timesteps = 12
 
@@ -51,39 +49,47 @@ def get_accuracy_pl_obs(df):
         print('----'+mode.upper()+'----')
         df_mode = df[df["mode"] == mode]
 
-        print("len(df_mode):", len(df_mode))
-        print(df_mode)
+        num_traces = len(df_mode["obs_filename_0"])
+        obs = {}
 
-        obs_filenames = np.concatenate([df_mode["obs_filename_" + str(i)] for i in range(timesteps)])
-        obs_filenames = [x.split('/')[-1] for x in obs_filenames]
+        for i in range(num_traces):
+            for j in range(timesteps):
 
-        print("len(obs_filenames):", len(obs_filenames))
-        print(obs_filenames)
+                file_name = df_mode["obs_filename_"+str(j)][i].split('/')[-1]
+                obs_name = file_name.split('_')[0]
 
-        expected = np.concatenate([df_mode["expected_label_" + str(i)] for i in range(timesteps)])
-        predicted = np.concatenate([df_mode["predicted_label_" + str(i)] for i in range(timesteps)])
+                if obs_name == 'r':
+                    expected_0 = df_mode["expected_label_"+str(j)][i]
+                    predicted_0 = df_mode["predicted_label_" + str(j)][i]
+                    obs[file_name] = (expected_0 == predicted_0)
 
-        for i, obs in enumerate(obs_filenames):
-            obs_name = obs.split('_')[0]
-            if obs_name == 'bg':
-                pass
-            elif obs_name == 'gb':
-                pass
-            elif obs_name == 'r':
-                pass
-            elif obs_name == 'rr':
-                pass
-            elif obs_name == 'rrr':
-                pass
+                elif obs_name == 'bg' or obs_name == 'gb' or obs_name == 'rr':
+                    expected_0 = df_mode["expected_label_"+str(j)][i]
+                    predicted_0 = df_mode["predicted_label_" + str(j)][i]
+                    expected_1 = df_mode["expected_label_"+str(j)][i + 1]
+                    predicted_1 = df_mode["predicted_label_" + str(j)][i + 1]
+                    obs[file_name] = (expected_0 == predicted_0) and (expected_1 == predicted_1)
 
+                elif obs_name == 'rrr':
+                    expected_0 = df_mode["expected_label_" + str(j)][i]
+                    predicted_0 = df_mode["predicted_label_" + str(j)][i]
+                    expected_1 = df_mode["expected_label_" + str(j)][i + 1]
+                    predicted_1 = df_mode["predicted_label_" + str(j)][i + 1]
+                    expected_2 = df_mode["expected_label_" + str(j)][i + 2]
+                    predicted_2 = df_mode["predicted_label_" + str(j)][i + 2]
+                    obs[file_name] = (expected_0 == predicted_0) and (expected_1 == predicted_1) and (expected_2 == predicted_2)
 
-        confusion_matrix = pd.crosstab(expected, predicted, rownames=['Expected'], colnames=['Predicted'])
-        print(confusion_matrix)
+        a_dict = {}
+        for k, v in obs.items():
+            obs_id = k.split('_')[0]
+            if obs_id not in a_dict:
+                a_dict[obs_id] = (0,0)
+            a_dict[obs_id][0] += v
+            a_dict[obs_id][1] += 1
 
-        accuracy = accuracy_score(y_true=expected, y_pred=predicted)
-        print("accuracy:", accuracy)
-
-
+        print("obs_accuracy:")
+        for k, v in a_dict.items():
+            print(k, v[0] / v[1])
 
 
 def parse_exec_args():
