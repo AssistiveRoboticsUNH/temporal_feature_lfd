12ea7f9cff402ffec8f7f432e10528c3b71ba89c
mbc2004 <mbc2004@cs.unh.edu>
2021-03-16 11:46:11 -0400

    end-to-end implementation

--- model/temporal/iad_2_itr_layers.py
+++ model/temporal/iad_2_itr_layers.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+from torch_geometric.nn import MessagePassing
 from torch_geometric.data import Data
 
 
@@ -75,6 +76,66 @@ To do so, it uses the indices returned by the forward pass."""
 
 
 class MaskedIAD2ITR(torch.autograd.Function):
+    """
+    We can implement our own custom autograd Functions by subclassing
+    torch.autograd.Function and implementing the forward and backward passes
+    which operate on Tensors.
+    """
+
+    @staticmethod
+    def forward(ctx, iad):
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the output. ctx is a context object that can be used
+        to stash information for backward computation. You can cache arbitrary
+        objects for use in the backward pass using the ctx.save_for_backward method.
+        """
+        ctx.save_for_backward(iad)
+
+        # take masked IAD
+        sparse_map = convert_iad_to_sparse_map(iad)
+
+        # generate set of nodes
+        # keep track of node weights to back-propagate the grad
+        itr = convert_sparse_map_to_itr(sparse_map, iad.detach().cpu().numpy())
+
+        # generate edges and ITRs
+        # ctx.save_for_backward(input)
+
+        # return Graph
+        #return itr
+        return itr.x, itr.edge_idx, itr.edge_attr
+
+        #ctx.save_for_backward(input)
+        #return input.clamp(min=0)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the loss
+        with respect to the output, and we need to compute the gradient of the loss
+        with respect to the input.
+        """
+
+        iad, _ = ctx.saved_tensors
+        node_grad = grad_output.clone()
+
+        grad_input = np.zeros_like(iad)
+
+        count = 0
+        for feature in sparse_map:
+            for start, stop in sparse_map[feature]:
+                grad_input[feature, start:stop] = node_grad[count]
+                count += 1
+
+        return grad_input
+
+        #input, = ctx.saved_tensors
+        #grad_input = grad_output.clone()
+        #grad_input[input < 0] = 0
+        #return grad_input
+
+class MaskedIAD2ITR_Message(MessagePassing):
     """
     We can implement our own custom autograd Functions by subclassing
     torch.autograd.Function and implementing the forward and backward passes
@@ -318,5 +379,7 @@ if __name__ == '__main__':
 
     masked_iad = IAD2MaskedIAD.apply(iad, threshold_values)
     print("masked_iad:", masked_iad)
-    itr = MaskedIAD2ITR.apply(masked_iad)
-    print("itr:", itr)
+    x, edge_idx, edge_attr = MaskedIAD2ITR_Message.apply(masked_iad)
+    print("x:", x)
+    #itr = MaskedIAD2ITR_Message.apply(masked_iad)
+    #print("itr:", itr)
