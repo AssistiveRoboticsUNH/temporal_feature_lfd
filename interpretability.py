80efa7411f799b7ff3bfeb1046a2963a30c26c73
mbc2004 <mbc2004@cs.unh.edu>
2021-03-18 13:01:39 -0400

    interpretability

--- interpretability.py
+++ interpretability.py
@@ -41,7 +41,7 @@ def convert_to_img(args, rgb_img, activation_map):
 
     img = rgb_img[0]
     for t in range(1, rgb_img.shape[0]):
-        img = np.concatentate(img, rgb_img[t])
+        img = np.concatenate(img, rgb_img[t])
 
     print("img.shape:", img.shape)
 
