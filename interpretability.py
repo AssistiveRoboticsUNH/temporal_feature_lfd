3fec7bbad06f648a924697c4ee63ff140d6896e0
mbc2004 <mbc2004@cs.unh.edu>
2021-03-22 10:53:07 -0400

    interpretability

--- interpretability.py
+++ interpretability.py
@@ -51,7 +51,7 @@ def convert_to_img(args, rgb_img, activation_map):
             #print("img_frame.size:", img_frame.size)
 
 
-            print(activation_map[f, t])
+            #print(activation_map[f, t])
             activation_frame = Image.fromarray(activation_map[f, t])
 
             activation_frame_dst = np.array(Image.new("HSV", activation_frame.size))
@@ -62,12 +62,12 @@ def convert_to_img(args, rgb_img, activation_map):
             activation_frame_dst[..., 2] = 100 #* (np.array(activation_frame) / 255.0)
             activation_frame_dst = np.array(Image.fromarray(activation_frame_dst, 'HSV').convert("RGBA"))
             activation_frame_dst[..., 3] = activation_frame#np.array(activation_frame) / 255.0
-            print("activation_frame_dst[0,0]:", activation_frame_dst[..., 3])
+            #print("activation_frame_dst[0,0]:", activation_frame_dst[..., 3])
             activation_frame_dst = Image.fromarray(activation_frame_dst, "RGBA").resize((width, height), PIL.Image.NEAREST)
 
             #activation_frame_dst.putalpha(128)
 
-            img_frame = activation_frame_dst#Image.alpha_composite(img_frame, activation_frame_dst)
+            img_frame = Image.alpha_composite(img_frame, activation_frame_dst)
 
 
             #print("img_frame:", img_frame.shape)
