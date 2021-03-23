6ebd73881f5606b6db3908f22636170811e80ea0
mbc2004 <mbc2004@cs.unh.edu>
2021-03-23 06:59:37 -0400

    interpretability

--- parameter_parser.py
+++ parameter_parser.py
@@ -98,6 +98,7 @@ def default_model_params():
 
                 elif app == "tea_making":
                     self.file_directory = "/home/mbc2004/datasets/TeaMaking2"
+                    self.trace_file = None
                     self.obs_label_list = {"add_milk": 0, "add_sugar": 1, "add_tea_bag": 2, "add_water": 3,
                                            "nothing": 4, "stir": 5, "toggle_on_off": 6}
                     self.act_label_list = None  # {"N": 0, "R": 1, "G": 2, "B": 3}
