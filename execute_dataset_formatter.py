54dbfadf8b2f784fe7ccd703c792f373bbd28994
mbc2004 <mbc2004@cs.unh.edu>
2021-03-28 22:37:28 -0400

    iad_dataset_formatter-jester_check

--- execute_dataset_formatter.py
+++ execute_dataset_formatter.py
@@ -134,7 +134,7 @@ def parse_exec_args():
     #parser.add_argument('app', help='classifier(c)/policy_learner(pl)', choices=['c', 'pl'])
     parser.set_defaults(app='c')
     parser.add_argument('model', help='model_id', choices=model_dict.keys())
-    parser.set_defaults(suffix='backbone')
+    parser.set_defaults(suffix='none')
     #arser.add_argument('suffix', help='suffix', choices=['backbone', 'linear', 'lstm', 'tcn', 'ditrl'])
 
     #parser.set_defaults(generate_files=False)
