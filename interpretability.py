c5e911d70a2b0ae58b32da6fd17a184522b30273
mbc2004 <mbc2004@cs.unh.edu>
2021-04-01 15:14:01 -0400

    global limit

--- interpretability.py
+++ interpretability.py
@@ -22,16 +22,23 @@ import torch
 from scipy.signal import savgol_filter
 
 
-def convert_to_img(args, rgb_img, activation_map, feature_ranking, max_features=5,
+def convert_to_img(args, filename, activation_map, feature_ranking, max_features=5,
                    min_v_global=None, max_v_global=None, avg_v_global=None):
 
     #rgb_img = np.array([Image.open(f) for f in os.listdir(filename)])
 
+    total_num_frames = len(os.listdir(filename))
+    idxs = np.linspace(0, max(1, total_num_frames - 1), num=64, dtype=int) + 1
+    rgb_img = [Image.open(os.path.join(filename, 'image_{:05d}.jpg'.format(idx))).convert('RGB').resize(224, 224) for idx in idxs]
+
+
     print("rgb_img.shape:", rgb_img.shape)
+    '''
     rgb_img = rgb_img.reshape([args.frames, 3, rgb_img.shape[-2], rgb_img.shape[-1]])
     rgb_img = rgb_img.transpose([0, 2, 3, 1])
     rgb_img *= 255
     rgb_img = rgb_img.astype(np.uint8)
+    '''
 
     num_frames, height, width = rgb_img.shape[0], rgb_img.shape[1], rgb_img.shape[2]
     num_features = activation_map.shape[1]
@@ -144,7 +151,7 @@ def exec_func(args, lfd_params):
                 activation_map = activation_map.detach().cpu().numpy()
                 print(activation_map.shape)
 
-                img_out = convert_to_img(args, obs, activation_map, feature_ranking=feature_ranking, max_features=args.max)
+                img_out = convert_to_img(args, filename, activation_map, feature_ranking=feature_ranking, max_features=args.max)
 
                 filename_split = filename.split('/')
                 filename_id = filename_split[-1].split('.')[0] + ".png"
