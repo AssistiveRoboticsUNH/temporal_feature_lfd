788192a71fb02331da88f25b1bdf923c3b637d8f
mbc2004 <mbc2004@cs.unh.edu>
2021-04-08 15:55:34 -0400

    fix interpretability bug

--- interpretability.py
+++ interpretability.py
@@ -82,8 +82,8 @@ def convert_to_img(args, filename, activation_map, feature_ranking, max_features
     print("activation_map.shape:", activation_map.shape)
 
     #dst = Image.new('RGB', (width * num_frames, height * num_features))
-    #dst = Image.new('RGB', (int(width/2) * num_frames, int(height/2) * max_features))
-    dst = Image.new('RGBA', (int(width / 2) * num_frames, int(height / 2)))
+    dst = Image.new('RGB', (int(width/2) * num_frames, int(height/2) * max_features))
+    #dst = Image.new('RGBA', (int(width / 2) * num_frames, int(height / 2)))
     #dst = Image.new('RGB', (width, height * num_features))
     #dst = Image.new('RGB', (width, height))
 
@@ -116,12 +116,13 @@ def convert_to_img(args, filename, activation_map, feature_ranking, max_features
 
             # combine images
             #am_dst.paste(activation_frame_dst, 0, 0))
-            am_dst = Image.alpha_composite(am_dst, activation_frame_dst)
+            #am_dst = Image.alpha_composite(am_dst, activation_frame_dst)
+            img_frame = Image.alpha_composite(img_frame, activation_frame_dst)
 
             # add to full image
-            #dst.paste(img_frame, (int(width/2) * t, int(height/2) * fi))
-        img_frame = Image.alpha_composite(img_frame, am_dst)
-        dst.paste(img_frame, (int(width / 2) * t, 0))
+            dst.paste(img_frame, (int(width/2) * t, int(height/2) * fi))
+        #img_frame = Image.alpha_composite(img_frame, am_dst)
+        #dst.paste(img_frame, (int(width / 2) * t, 0))
         #print("paste @: ", (int(width / 2) * t, 0))
 
     return dst
