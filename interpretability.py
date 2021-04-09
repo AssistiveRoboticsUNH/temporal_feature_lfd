78dd5833d42746bf6da531fd34f0ee50a0d8374f
mbc2004 <mbc2004@cs.unh.edu>
2021-04-09 13:24:32 -0400

    fix interpretability bug

--- interpretability.py
+++ interpretability.py
@@ -119,7 +119,7 @@ def convert_to_img(args, filename, activation_map, feature_ranking, max_features
 
                 # apply activation map as alpha channel
                 activation_frame_dst = np.array(Image.fromarray(activation_frame_dst, 'HSV').convert("RGBA"))
-                activation_frame_dst[..., 3] = activation_frame
+                activation_frame_dst[..., 3] = int(255/2.0)#activation_frame
                 activation_frame_dst = Image.fromarray(activation_frame_dst, "RGBA").resize((int(width/2), int(height/2)), PIL.Image.NEAREST)
 
                 # combine images
