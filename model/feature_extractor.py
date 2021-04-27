bf2132ee42d7adfbb220ebf68d2edbc59d1a6575
MClarkTurner <mbc2004@cs.unh.edu>
2021-04-27 06:37:25 -0400

    mn2 and jester

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -37,7 +37,7 @@ class FeatureExtractor(nn.Module):
             self.backbone = backbone_class(self.lfd_params,
                                            is_training=self.backbone_train,
                                            trim_model=use_bottleneck,
-                                           filename=pretrain_model_name if self.backbone_train else self.backbone_filename,
+                                           filename=pretrain_model_name,# if self.backbone_train else self.backbone_filename,
                                            end_point=lfd_params.model.end_point)
 
         if self.use_bottleneck:
