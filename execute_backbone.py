b5bd2fc69361da401d817b26cd6f293b958f041a
MClarkTurner <mbc2004@cs.unh.edu>
2021-06-28 13:45:58 -0400

    work on crepe dataset

--- execute_backbone.py
+++ execute_backbone.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     lfd_params = default_model_params()
     #lfd_params.set_application("block_construction")
     lfd_params.set_application(args.application)
-    for model in ['tsm', 'i3d', 'wrn']:
+    for model in ['wrn']:
         lfd_params.set_model_params(model_dict[model], end_point=-1)
 
     #lfd_params.set_model_params(model_dict[args.model], end_point=-1)
