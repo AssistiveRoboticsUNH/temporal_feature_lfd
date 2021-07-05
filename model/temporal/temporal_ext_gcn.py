a91f4c361a64fb5c238fa85f19dda218bad474c2
MClarkTurner <mbc2004@cs.unh.edu>
2021-07-05 12:53:02 -0400

    work on crepe dataset

--- model/temporal/temporal_ext_gcn.py
+++ model/temporal/temporal_ext_gcn.py
@@ -38,8 +38,8 @@ class TemporalExtGCN(nn.Module):
 
         self.gcn1 = RGCNConv(self.node_size, self.hidden_size, num_relations=self.num_relations)
         self.gcn2 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)
-        #self.gcn3 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)
-        #self.gcn4 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)
+        self.gcn3 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)#
+        self.gcn4 = RGCNConv(self.hidden_size, self.hidden_size, num_relations=self.num_relations)#
         #self.densegcn = gnn.DenseGCNConv(self.hidden_size, self.output_size)
         #nn1 = nn.Sequential(nn.Linear(self.node_size, self.hidden_size), nn.ReLU(), nn.Linear(self.hidden_size, self.hidden_size))
         #self.gcn = GINConv(nn1)
@@ -70,6 +70,8 @@ class TemporalExtGCN(nn.Module):
 
         x = F.relu(self.gcn1(x, edge_idx, edge_attr))
         x = F.relu(self.gcn2(x, edge_idx, edge_attr))
+        x = F.relu(self.gcn3(x, edge_idx, edge_attr))#
+        x = F.relu(self.gcn4(x, edge_idx, edge_attr))#
         x = gnn.global_add_pool(x, batch)
 
         x = self.fc(x)
