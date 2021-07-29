fa264f37deacb4a047ee5bfb5dc725d432614aef
MClarkTurner <mbc2004@cs.unh.edu>
2021-07-29 14:44:22 -0400

    train ikea_fa

--- analysis/model_analyze.py
+++ analysis/model_analyze.py
@@ -35,7 +35,8 @@ def get_accuracy_c(args, df):
 
             df_cm = pd.DataFrame(cm, index=class_labels, columns=class_labels)
             plt.figure(figsize=(10, 7))
-            plot = sns.heatmap(df_cm, annot=True, cmap="YlGnBu")
+            plot = sns.heatmap(df_cm, annot=True, cmap="YlGnBu", vmax=1)
+            plot.set_yticklabels(plot.get_yticklabels(), rotation=0)
             plot.figure.savefig(os.path.join(args.model,"cm_"+mode+".png"))
 
 
