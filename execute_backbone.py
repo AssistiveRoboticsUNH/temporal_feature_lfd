57bb6255134164488477ef13e24a676bd15e3b24
MClarkTurner <mbc2004@cs.unh.edu>
2021-08-02 21:46:20 -0400

    train crepe

--- execute_backbone.py
+++ execute_backbone.py
@@ -38,10 +38,10 @@ if __name__ == '__main__':
     #lfd_params.set_application("block_construction")
     lfd_params.set_application(args.application)
 
-    # lfd_params.set_model_params(model_dict[args.model], end_point=-1)
-    for model in ['tsm', 'wrn']:
-        lfd_params.set_model_params(model_dict[model], end_point=-1)
+    lfd_params.set_model_params(model_dict[args.model], end_point=-1)
+    #for model in ['tsm', 'wrn']:
+    #    lfd_params.set_model_params(model_dict[model], end_point=-1)
 
-        lfd_params.input_frames = args.frames
+    lfd_params.input_frames = args.frames
 
-        exec_different_bottleneck_sizes(args, lfd_params)
+    exec_different_bottleneck_sizes(args, lfd_params)
